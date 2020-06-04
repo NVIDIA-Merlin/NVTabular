8d3247151a102551279eac178b336e421feb18a9
Alec Gunny <amacgunny@gmail.com>
2020-06-04 12:38:07 -0700

    [REVIEW] Adding Hash Bucket Op (#7)
    * added hash op
    
    * adding hash_bucket test
    
    * tests pass
    
    * added hash op
    
    * blackened ops
    
    * blackened groupby
    
    * flake8 fixes
    
    * getting rid of extraneous hashbucket
    
    Co-authored-by: Alec Gunny <agunny@nvidia.com>
    Co-authored-by: Ben Frederickson <github@benfrederickson.com>

--- nvtabular/ops.py
+++ nvtabular/ops.py
@@ -617,6 +617,43 @@ class LogOp(TransformOperator):
         return new_gdf
 
 
+class HashBucket(TransformOperator):
+    default_in = CAT
+    default_out = CAT
+
+    def __init__(self, num_buckets, columns=None, **kwargs):
+        if isinstance(num_buckets, dict):
+            columns = [i for i in num_buckets.keys()]
+            self.num_buckets = num_buckets
+        elif isinstance(num_buckets, (tuple, list)):
+            assert columns is not None
+            assert len(columns) == len(num_buckets)
+            self.num_buckets = {col: nb for col, nb in zip(columns, num_buckets)}
+        elif isinstance(num_buckets, int):
+            self.num_buckets = num_buckets
+        else:
+            raise TypeError(
+                "`num_buckets` must be dict, iterable, or int, got type {}".format(
+                    type(num_buckets)
+                )
+            )
+        super(HashBucket, self).__init__(columns=columns, **kwargs)
+
+    @annotate("HashBucket_op", color="darkgreen", domain="nvt_python")
+    def op_logic(self, gdf: cudf.DataFrame, target_columns: list, stats_context=None):
+        cat_names = target_columns
+        if isinstance(self.num_buckets, int):
+            num_buckets = {name: self.num_buckets for name in cat_names}
+        else:
+            num_buckets = self.num_buckets
+
+        new_gdf = cudf.DataFrame()
+        for col, nb in num_buckets.items():
+            new_col = f"{col}_{self._id}"
+            new_gdf[new_col] = gdf[col].hash_values() % nb
+        return new_gdf
+
+
 class Normalize(DFOperator):
     """
     Standardizing the features around 0 with a standard deviation
