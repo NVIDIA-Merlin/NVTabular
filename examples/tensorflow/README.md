68957b51fbd8f8ab6d7aa06b8114758b8524a635
Alec Gunny <agunny@nvidia.com>
2020-07-01 13:54:56 -0700

    notebook updates and addding image from run

---
+++ examples/tensorflow/README.md
@@ -0,0 +1,1 @@
+<img src="dlrm-train.PNG"></img>
---
+++ examples/tensorflow/callbacks.py
@@ -0,0 +1,21 @@
+import tensorflow as tf
+import time
+
+
+class ThroughputLogger(tf.keras.callbacks.Callback):
+    def __init__(self, batch_size, window_size=10, **kwargs):
+        self.batch_size = batch_size
+        self.window_size = window_size
+        self.times = []
+        super(ThroughputLogger, self).__init__(**kwargs)
+
+    def on_epoch_begin(self, epoch, logs=None):
+        self.times = [time.time()]
+
+    def on_batch_end(self, batch, logs=None):
+        self.times.append(time.time())
+        if len(self.times) > self.window_size:
+            del self.times[0]
+
+        time_delta = self.times[-1] - self.times[0]
+        logs['throughput'] = self.batch_size*len(self.times) / time_delta
