eb639426eb1ef15d272702dcf26d5c56085f3444
bschifferer <bschifferer@nvidia.com>
2020-07-02 19:37:45 -0400

    adding op support for string, timeseries and astype (#84)

--- nvtabular/ops.py
+++ nvtabular/ops.py
@@ -95,8 +95,6 @@ class TransformOperator(Operator):
 
         new_key = self._id
 
-        if not pro:
-            input_cols = self.default_out
         columns_ctx[input_cols][new_key] = []
         if self.replace and self.preprocessing:
             # not making new columns instead using old ones
@@ -780,7 +778,6 @@ class NormalizeMinMax(DFOperator):
 
 
 class FillMissing(DFOperator):
-
     """
     This operation replaces missing values with a constant pre-defined value
 
@@ -821,7 +818,6 @@ class FillMissing(DFOperator):
 class FillMedian(DFOperator):
     """
     This operation replaces missing values with the median value for the column.
-
     Although you can directly call methods of this class to
     transform your continuous features, it's typically used within a
     Workflow class.
@@ -1079,7 +1075,6 @@ class GroupBy(DFOperator):
 
 
 class Categorify(DFOperator):
-
     """
     Most of the data set will contain categorical features,
     and these variables are typically stored as text values.
@@ -1214,6 +1209,7 @@ def get_embedding_order(cat_names):
 def get_embedding_size(encoders, cat_names):
     """ Returns a suggested size of embeddings based off cardinality of encoding categorical
     variables
+
     Parameters
     -----------
     encoders : dict
@@ -1228,3 +1224,49 @@ def get_embedding_size(encoders, cat_names):
 
 def _emb_sz_rule(n_cat: int) -> int:
     return n_cat, int(min(16, round(1.6 * n_cat ** 0.56)))
+
+
+class LambdaOp(TransformOperator):
+    """
+    Enables to call Methods to cudf.Series
+
+    Parameters
+    -----------
+    op_name : str
+        name of the operator column. It is used as a post_fix for the
+        modified column names (if replace=False)
+    f : lambda function
+        defines the function executed on dataframe level, expectation is lambda col, gdf: ...
+        col is the cudf.Series defined by the context
+        gdf is the full cudf.DataFrame
+    columns :
+    preprocessing : bool, default True
+        Sets if this is a pre-processing operation or not
+    replace : bool, default True
+        Replaces the transformed column with the original input
+        if set Yes
+    """
+
+    default_in = ALL
+    default_out = ALL
+
+    def __init__(self, op_name, f, columns=None, preprocessing=True, replace=True):
+        super().__init__(columns=columns, preprocessing=preprocessing, replace=replace)
+        if op_name is None:
+            raise ValueError("op_name cannot be None. It is required for naming the column.")
+        if f is None:
+            raise ValueError("f cannot be None. LambdaOp op applies f to dataframe")
+        self.f = f
+        self.op_name = op_name
+
+    @property
+    def _id(self):
+        return str(self.op_name)
+
+    @annotate("DFLambda_op", color="darkgreen", domain="nvt_python")
+    def op_logic(self, gdf: cudf.DataFrame, target_columns: list, stats_context=None):
+        new_gdf = cudf.DataFrame()
+        for col in target_columns:
+            new_gdf[col] = self.f(gdf[col], gdf)
+        new_gdf.columns = [f"{col}_{self._id}" for col in new_gdf.columns]
+        return new_gdf
