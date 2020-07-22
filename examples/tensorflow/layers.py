b26b293c5df8452c4c044c6f372cd51c4dc2e474
Alec Gunny <agunny@nvidia.com>
2020-07-21 20:59:42 -0700

    cleaning up notebook and layers code, adding cupti symlink to Dockerfile

--- examples/tensorflow/layers.py
+++ examples/tensorflow/layers.py
@@ -95,6 +95,12 @@ class DotProductInteraction(tf.keras.layers.Layer):
             output_dim = input_shape[1] * (input_shape[1] - 1) // 2
         return (input_shape[0], output_dim)
 
+    def get_config(self):
+        return {
+            "interaction_type": self.interaction_type,
+            "self_interaction": self.self_interaction
+        }
+
 
 def _sort_columns(feature_columns):
     return sorted(feature_columns, key=lambda col: col.name)
@@ -127,97 +133,100 @@ def _validate_categorical_column(feature_column):
         )
 
 
+def _validate_stack_dimensions(feature_columns):
+    dims = []
+    for feature_column in feature_columns:
+        try:
+            dimension = feature_column.dimension
+        except AttributeError:
+            dimension = feature_column.shape[0]
+        dims.append(dimension)
+
+    dim0 = dims[0]
+    if not all([dim == dim0 for dim in dims[1:]]):
+        raise ValueError(
+            "'stack' aggregation requires all categorical "
+            "embeddings and continuous features to have same "
+            "size. Found dimensions {}".format(dims)
+        )
+
+
+def _validate_feature_columns(feature_columns):
+    # TODO: check everything and then raise errors at the end
+    for feature_column in feature_columns:
+        if isinstance(feature_column, fc.CategoricalColumn):
+            # TODO: I think this technically excludes BucketizedColumns,
+            # which could in theory be treated numerically
+            raise ValueError(
+                "All feature columns must be dense! Please wrap "
+                "categorical columns in embedding or indicator "
+                "columns before passing"
+            )
+        elif isinstance(feature_column, (fc.EmbeddingColumn, fc.IndicatorColumn)):
+            _validate_categorical_column(feature_column.categorical_column)
+
+        elif isinstance(feature_column, fc.NumericColumn):
+            _validate_numeric_column(feature_column)
+
+
 class ScalarDenseFeatures(tf.keras.layers.Layer):
     '''
     Layer which maps scalar numeric and one-hot categorical
     features to a dense embedding.
     '''
     def __init__(self, feature_columns, aggregation="concat", name=None, **kwargs):
-        numeric_features = []
-        embedding_shapes = {}
-
-        # sort feature columns to make layer independent of column
-        # feed order
+        # sort feature columns to make layer independent of column order
         feature_columns = _sort_columns(feature_columns)
-
-        # TODO: check everything and then raise errors at the end
-        for feature_column in feature_columns:
-            if isinstance(feature_column, fc.CategoricalColumn):
-                raise ValueError(
-                    "All feature columns must be dense! Please wrap "
-                    "categorical columns in embedding or indicator "
-                    "columns before passing"
-                )
-            elif isinstance(feature_column, (fc.EmbeddingColumn, fc.IndicatorColumn)):
-                _validate_categorical_column(feature_column.categorical_column)
-                feature_name = feature_column.categorical_column.key
-                num_buckets = feature_column.categorical_column.num_buckets
-
-                # for embedding, record both dimensions, otherwise only need
-                # one dim for indicator
-                if isinstance(feature_column, fc.EmbeddingColumn):
-                    embedding_shapes[feature_name] = (
-                        num_buckets,
-                        feature_column.dimension,
-                    )
-                else:
-                    embedding_shapes[feature_name] = (num_buckets,)
-
-            elif isinstance(feature_column, fc.NumericColumn):
-                if aggregation == "stack":
-                    raise ValueError(
-                        "Stacked aggregation not compatible with numeric columns"
-                    )
-                _validate_numeric_column(feature_column)
-                numeric_features.append(feature_column.key)
+        _validate_feature_columns(feature_columns)
 
         assert aggregation in ("concat", "stack")
         if aggregation == "stack":
-            embedding_dims = [i[-1] for i in embedding_shapes.values()]
-            embedding_dim = embedding_dims[0]
-            assert all([i == embedding_dim for i in embedding_dims[1:]])
+            _validate_stack_dimensions(feature_columns)
 
-        self.numeric_features = numeric_features
-        self.embedding_shapes = embedding_shapes
+        self.feature_columns = feature_columns
         self.aggregation = aggregation
-
         super(ScalarDenseFeatures, self).__init__(name=name, **kwargs)
 
     def build(self, input_shapes):
         assert all([shape[1] == 1 for shape in input_shapes.values()])
 
         self.embedding_tables = {}
-        for feature_name, embedding_shape in self.embedding_shapes.items():
-            if len(embedding_shape) == 2:
+        for feature_column in self.feature_columns:
+            if isinstance(feature_column, fc.NumericColumn):
+                continue
+
+            feature_name = feature_column.categorical_column.key
+            num_buckets = feature_column.categorical_column.num_buckets
+            if isinstance(feature_column, fc.EmbeddingColumn):
                 self.embedding_tables[feature_name] = self.add_weight(
                     name="{}/embedding_weights".format(feature_name),
                     trainable=True,
                     initializer="glorot_normal",
-                    shape=embedding_shape,
+                    shape=(num_buckets, feature_column.dimension),
                 )
             else:
                 self.embedding_tables[feature_name] = self.add_weight(
                     name="{}/embedding_weights".format(feature_name),
                     trainable=False,
-                    initializer=tf.constant_initializer(np.eye(embedding_shape[0])),
-                    shape=(embedding_shape[0], embedding_shape[0]),
+                    initializer=tf.constant_initializer(np.eye(num_buckets)),
+                    shape=(num_buckets, num_buckets)
                 )
         self.built = True
 
     def call(self, inputs):
-        dense_features = [
-            inputs[feature_name] for feature_name in self.numeric_features
-        ]
-
-        categorical_feature_names = sorted(self.embedding_shapes.keys())
-        for feature_name in categorical_feature_names:
-            table = self.embedding_tables[feature_name]
-            embeddings = tf.gather(table, inputs[feature_name][:, 0])
-            dense_features.append(embeddings)
-
+        features = []
+        for feature_column in self.feature_columns:
+            if isinstance(feature_column, fc.NumericColumn):
+                features.append(inputs[feature_column.name])
+            else:
+                feature_name = feature_column.categorical_column.name
+                table = self.embedding_tables[feature_name]
+                embeddings = tf.gather(table, inputs[feature_name][:, 0])
+                features.append(embeddings)
+    
         if self.aggregation == "stack":
-            return tf.stack(dense_features, axis=1)
-        return tf.concat(dense_features, axis=1)
+            return tf.stack(features, axis=1)
+        return tf.concat(features, axis=1)
 
     def compute_output_shape(self, input_shapes):
         input_shape = [i for i in input_shapes.values()][0]
@@ -229,3 +238,9 @@ class ScalarDenseFeatures(tf.keras.layers.Layer):
         else:
             embedding_dim = [i for i in self.embedding_shapes.values()][0]
             return (input_shape[0], len(self.embedding_shapes), embedding_dim)
+
+    def get_config(self):
+        return {
+            "feature_columns": self.feature_columns,
+            "aggregation": self.aggregation
+        }
