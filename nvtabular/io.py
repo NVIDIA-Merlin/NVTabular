eacec8791bf04a99e92e1c823bf9821d62c53be4
Richard (Rick) Zamora <rzamora217@gmail.com>
2020-07-24 15:42:49 -0500

    Minor fix to avoid extra (unnecessary) shuffle (#169)

--- nvtabular/io.py
+++ nvtabular/io.py
@@ -253,7 +253,8 @@ class ThreadedWriter(Writer):
     def add_data(self, gdf):
 
         # Shuffle if necessary
-        if self.shuffle:
+        # (Skip shuffle if "full", because we will do it later)
+        if self.shuffle and self.shuffle != "full":
             gdf = _shuffle_gdf(gdf)
 
         # Populate columns idxs
@@ -481,9 +482,6 @@ def _write_output_partition(
 ):
     gdf_size = len(gdf)
     out_files_per_proc = out_files_per_proc or 1
-    if shuffle and shuffle != "full":
-        # We should do a real sort here
-        gdf = _shuffle_gdf(gdf, gdf_size=gdf_size)
 
     # Get cached writer (or create/cache a new one)
     with get_worker_cache("writer") as writer_cache:
