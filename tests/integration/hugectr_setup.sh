78bf4b76b9bc454500b624d86a7628b7d3ec3042
Alberto Alvarez <albertoa@nvidia.com>
2020-08-04 10:25:18 -0700

    Notebook works

--- tests/integration/hugectr_setup.sh
+++ tests/integration/hugectr_setup.sh
@@ -5,6 +5,12 @@ VAL_MODE=OFF
 ENABLE_MULTINODES=OFF
 NCCL_A2A=ON
 
+# Install packages
+pip install jupyterlab 
+pip install pynvml
+pip install torch torchvision
+
+# Install HugeCTR
 git clone -b cgarg/parquet-v2.2-integ https://gitlab-master.nvidia.com/zehuanw/hugectr.git &&\
     cd hugectr && \
     git submodule update --init --recursive && \
