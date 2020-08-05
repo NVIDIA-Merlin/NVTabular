5c98118f4e66034f3f2a4a7b7d9d2b00cca2f2b6
Julio Perez <jperez@nvidia.com>
2020-08-05 03:54:37 +0000

    reformatting for effect

--- tests/int/test_notebooks.py
+++ tests/int/test_notebooks.py
@@ -1,21 +1,22 @@
 import itertools
 import json
 import os
+import shutil
 import subprocess
 import sys
 from os.path import dirname, realpath
-import shutil
 
 import cudf
 import pytest
 
 TEST_PATH = dirname(dirname(realpath(__file__)))
-DATA_START = os.environ.get('DATASET_DIR', "/raid/data")
+DATA_START = os.environ.get("DATASET_DIR", "/raid/data")
+
 
 def test_criteo_notebook(tmpdir):
     input_path = os.path.join(DATA_START, "criteo/crit_int_pq")
     output_path = os.path.join(DATA_START, "criteo/crit_test")
-    
+
     _run_notebook(
         tmpdir,
         os.path.join(dirname(TEST_PATH), "examples", "criteo-example.ipynb"),
@@ -27,52 +28,36 @@ def test_criteo_notebook(tmpdir):
         batch_size=200000,
     )
 
-    
+
 def test_optimize_criteo(tmpdir):
     input_path = os.path.join(DATA_START, "criteo/crit_orig")
     output_path = os.path.join(DATA_START, "criteo/crit_test_opt")
 
-
     notebook_path = os.path.join(dirname(TEST_PATH), "examples", "optimize_criteo.ipynb")
     _run_notebook(
-        tmpdir, 
-        notebook_path,
-        input_path,
-        output_path,
-        gpu_id=2,
+        tmpdir, notebook_path, input_path, output_path, gpu_id=2,
     )
 
-    
 
 def test_rossman_example(tmpdir):
     pytest.importorskip("tensorflow")
     data_path = os.path.join(DATA_START, "rossman/data")
     input_path = os.path.join(DATA_START, "rossman/input")
     output_path = os.path.join(DATA_START, "rossman/output")
-    
+
     notebookpre_path = os.path.join(
         dirname(TEST_PATH), "examples", "rossmann-store-sales-preproc.ipynb"
     )
-    
+
     _run_notebook(
-        tmpdir, 
-        notebookpre_path, 
-        data_path,
-        input_path,
-        gpu_id=1,
-        clean_up=False,
-    )    
-    
+        tmpdir, notebookpre_path, data_path, input_path, gpu_id=1, clean_up=False,
+    )
 
     notebookex_path = os.path.join(
         dirname(TEST_PATH), "examples", "rossmann-store-sales-example.ipynb"
     )
     _run_notebook(
-        tmpdir, 
-        notebookex_path, 
-        input_path,
-        output_path,
-        gpu_id=1,
+        tmpdir, notebookex_path, input_path, output_path, gpu_id=1,
     )
 
 
@@ -81,17 +66,19 @@ def test_gpu_benchmark(tmpdir):
     output_path = os.path.join(DATA_START, "outbrains/output")
 
     notebook_path = os.path.join(dirname(TEST_PATH), "examples", "gpu_benchmark.ipynb")
-    _run_notebook(
-        tmpdir, 
-        notebook_path,
-        input_path,
-        output_path,
-        gpu_id=0,
-        batch_size=100000
-    )
-    
+    _run_notebook(tmpdir, notebook_path, input_path, output_path, gpu_id=0, batch_size=100000)
 
-def _run_notebook(tmpdir, notebook_path, input_path, output_path, batch_size=None, gpu_id=0, clean_up=True, transform=None):
+
+def _run_notebook(
+    tmpdir,
+    notebook_path,
+    input_path,
+    output_path,
+    batch_size=None,
+    gpu_id=0,
+    clean_up=True,
+    transform=None,
+):
     os.environ["CUDA_VISIBLE_DEVICES"] = str(gpu_id)
 
     if not os.path.exists(input_path):
@@ -100,7 +87,7 @@ def _run_notebook(tmpdir, notebook_path, input_path, output_path, batch_size=Non
         os.makedirs(output_path)
     if batch_size:
         os.environ["BATCH_SIZE"] = str(batch_size)
-    
+
     os.environ["INPUT_DATA_DIR"] = input_path
     os.environ["OUTPUT_DATA_DIR"] = output_path
     # read in the notebook as JSON, and extract a python script from it
@@ -120,7 +107,7 @@ def _run_notebook(tmpdir, notebook_path, input_path, output_path, batch_size=Non
     with open(script_path, "w") as script:
         script.write("\n".join(lines))
     subprocess.check_output([sys.executable, script_path])
-    
+
     # clear out products
     if clean_up:
         shutil.rmtree(output_path)
