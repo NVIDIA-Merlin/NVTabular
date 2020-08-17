0a719a28c7d2bb3e05b548fbc317717759b7c89e
Ben Frederickson <github@benfrederickson.com>
2020-08-17 12:23:32 -0700

    Add a ColumnSimilarity op (#213)
    Add a ColumnSimilarity op, that calculates the similarity between two columns
    using tf-idf, cosine or inner product as the distance metric. For each row,
    this calculates the distance between the two columns by looking up features
    for those columns in a sparse matrix, and then computing the distance
    between the rows of the feature matrices.

---
+++ tests/unit/test_column_similarity.py
@@ -0,0 +1,45 @@
+#
+# Copyright (c) 2020, NVIDIA CORPORATION.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+import cudf
+import cupy
+import pytest
+
+from nvtabular.column_similarity import ColumnSimilarity
+
+
+@pytest.mark.parametrize("on_device", [True, False])
+@pytest.mark.parametrize("metric", ["tfidf", "cosine", "inner"])
+def test_column_similarity(on_device, metric):
+    categories = cupy.sparse.coo_matrix(
+        (
+            cupy.ones(10),
+            (
+                cupy.array([0, 0, 0, 1, 1, 1, 2, 2, 2, 3]),
+                cupy.array([0, 1, 2, 1, 2, 3, 3, 4, 5, 1]),
+            ),
+        )
+    )
+
+    op = ColumnSimilarity("output", "left", categories, "right", metric=metric, on_device=on_device)
+    df = op.apply_op(cudf.DataFrame({"left": [0, 0, 0, 0], "right": [0, 1, 2, 3]}), None, None)
+
+    output = df.output.values
+    if metric in ("tfidf", "cosine"):
+        # distance from document 0 to itself should be 1, since these metrics are fully normalized
+        assert float(output[0]) == pytest.approx(1)
+
+    # distance from document 0 to document 2 should be 0 since they have no features in common
+    assert output[2] == 0
