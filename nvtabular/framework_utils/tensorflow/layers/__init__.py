887a853f27a3d789d628acda72bba145204ec59b
Alec Gunny <agunny@nvidia.com>
2020-09-09 09:41:31 -0700

    adding __all__ to layers and isorting

--- nvtabular/framework_utils/tensorflow/layers/__init__.py
+++ nvtabular/framework_utils/tensorflow/layers/__init__.py
@@ -1,2 +1,4 @@
 from embedding import ScalarDenseFeatures, ScalarLinearFeatures
 from interaction import DotProductInteraction
+
+__all__ = [ScalarLinearFeatures, ScalarDenseFeatures, DotProductInteraction]
--- nvtabular/framework_utils/tensorflow/layers/embedding.py
+++ nvtabular/framework_utils/tensorflow/layers/embedding.py
@@ -1,5 +1,4 @@
 import numpy as np
-
 import tensorflow as tf
 from tensorflow.python.feature_column import feature_column_v2 as fc
 
