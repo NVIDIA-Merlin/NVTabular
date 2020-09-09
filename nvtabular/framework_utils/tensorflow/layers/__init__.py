d78800f28fd40c758364299e0cb926b1071ce905
Alec Gunny <agunny@nvidia.com>
2020-09-09 11:30:04 -0700

    updating init

--- nvtabular/framework_utils/tensorflow/layers/__init__.py
+++ nvtabular/framework_utils/tensorflow/layers/__init__.py
@@ -14,7 +14,7 @@
 # limitations under the License.
 #
 
-from embedding import ScalarDenseFeatures, ScalarLinearFeatures
-from interaction import DotProductInteraction
+from .embedding import ScalarDenseFeatures, ScalarLinearFeatures
+from .interaction import DotProductInteraction
 
 __all__ = [ScalarLinearFeatures, ScalarDenseFeatures, DotProductInteraction]
