b5b5627b3b600eb477b998042d1c41c9630c27cb
Ben Frederickson <github@benfrederickson.com>
2020-09-09 21:26:08 -0700

    Update CHANGELOG and bump version for 0.2 release

--- setup.py
+++ setup.py
@@ -19,14 +19,14 @@ from setuptools import find_packages, setup
 
 setup(
     name="nvtabular",
-    version="0.1.1",
+    version="0.2.0",
     packages=find_packages(),
     url="https://github.com/NVIDIA/NVTabular",
     author="NVIDIA Corporation",
     license="Apache 2.0",
     long_description=open("README.md", encoding="utf8").read(),
     classifiers=[
-        "Development Status :: 3 - Alpha",
+        "Development Status :: 4 - Beta",
         "Programming Language :: Python :: 3",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: Apache Software License",
