5a065a53444d18753836c06386a59d7107f8c139
Alberto Alvarez <albertoa@nvidia.com>
2020-09-24 12:53:50 -0700

    Initial Docker support

---
+++ examples/hugectr/docker/hugectr_setup.sh
@@ -0,0 +1,20 @@
+# HugeCTR configuration
+CMAKE_BUILD_TYPE=Release
+SM="60;70;80"
+VAL_MODE=OFF
+ENABLE_MULTINODES=OFF
+NCCL_A2A=ON
+
+# Install HugeCTR
+git clone --depth 1 --branch "v2.2.1" https://github.com/NVIDIA/HugeCTR &&\
+    cd hugectr && \
+    git submodule update --init --recursive && \
+    mkdir build && cd build && \
+    cmake -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} -DSM=$SM .. && \
+    make -j && \
+    mkdir /usr/local/hugectr && \
+    make install && \
+    chmod +x /usr/local/hugectr/bin/* && \
+    rm -rf HugeCTR 
+
+PATH=/usr/local/hugectr/bin:$PATH 
