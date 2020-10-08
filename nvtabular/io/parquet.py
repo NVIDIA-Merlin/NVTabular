7d5d569d4bc3f630fd0a03548202ab8a24a8fed0
rjzamora <rzamora217@gmail.com>
2020-10-08 15:19:26 -0700

    save state

--- nvtabular/io/parquet.py
+++ nvtabular/io/parquet.py
@@ -101,6 +101,7 @@ class ParquetDatasetEngine(DatasetEngine):
         return dask_cudf.read_parquet(
             self.paths,
             columns=columns,
+            index=False,
             gather_statistics=False,
             split_row_groups=self.row_groups_per_part,
             storage_options=self.storage_options,
