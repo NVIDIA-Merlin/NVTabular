15805e8df9223152a7e3c3bf94c96cc542ab41cb
Ben Frederickson <github@benfrederickson.com>
2020-11-07 17:27:30 -0800

    Fix for workflows that generate new columns (#410)
    Generating a new column in the workflow and then using that in a future op
    wasn't working properly (#409). This adds a quick fix to revert to the previous
    behaviour of not limiting columns in the read_parquet call.

--- nvtabular/workflow.py
+++ nvtabular/workflow.py
@@ -585,8 +585,10 @@ class Workflow(BaseWorkflow):
         if self.ddf is None:
             raise ValueError("No dask_cudf frame available.")
         elif isinstance(self.ddf, Dataset):
-            columns = self.columns_ctx["all"]["base"]
-            return self.ddf.to_ddf(columns=columns, shuffle=self._shuffle_parts)
+            # Right now we can't distinguish between input columns and generated columns
+            # in the dataset, we don't limit the columm set right now in the to_ddf call
+            # (https://github.com/NVIDIA/NVTabular/issues/409 )
+            return self.ddf.to_ddf(shuffle=self._shuffle_parts)
         return self.ddf
 
     @staticmethod
