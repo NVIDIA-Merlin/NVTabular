339234717be0efd9d54050f4ac476bf4b0a287b4
Julio Perez <jperez@nvidia.com>
2020-12-11 01:17:31 +0000

    criteo setup complete

--- tests/integration/test_notebooks.py
+++ tests/integration/test_notebooks.py
@@ -30,15 +30,15 @@ import pytest
 from asvdb import BenchmarkResult
 
 TEST_PATH = dirname(dirname(realpath(__file__)))
-DATA_START = os.environ.get("DATASET_DIR", "/raid/data")
+DATA_START = os.environ.get("DATASET_DIR", "/raid/criteo")
 
 
-def test_criteo_notebook(tmpdir):
-    input_path = os.path.join(DATA_START, "criteo/crit_int_pq")
-    output_path = os.path.join(DATA_START, "criteo/crit_test")
+def test_criteo_notebook(db, bench_info, tmpdir):
+    input_path = os.path.join(DATA_START, "tests/crit_int_pq")
+    output_path = os.path.join(DATA_START, "tests/crit_test")
     os.environ["PARTS_PER_CHUNK"] = "1"
 
-    _run_notebook(
+    out = _run_notebook(
         tmpdir,
         os.path.join(dirname(TEST_PATH), "examples", "criteo-example.ipynb"),
         input_path,
@@ -48,6 +48,9 @@ def test_criteo_notebook(tmpdir):
         gpu_id=0,
         batch_size=100000,
     )
+    bench_results = CriteoBenchFastAI().get_epochs(out.splitlines())
+    send_results(db, bench_info, bench_results)
+    
 
 
 def test_criteohugectr_notebook(tmpdir):
@@ -97,18 +100,16 @@ def test_rossman_example(tmpdir, bench_info, db):
         dirname(TEST_PATH), "examples/rossmann", "rossmann-store-sales-fastai.ipynb"
     )
     out = _run_notebook(tmpdir, notebookex_path, input_path, output_path, gpu_id=1)
-    bench_results = bench_fastai("rossmann").get_epochs(out.splitlines())
-    for results in bench_results:
-        for result in results:
-            db.addResult(bench_info, result)
+    bench_results = RossBenchFastAI().get_epochs(out.splitlines())
+    send_results(db, bench_info, bench_results)
 
 
-def test_gpu_benchmark(tmpdir):
-    input_path = os.path.join(DATA_START, "outbrains/input")
-    output_path = os.path.join(DATA_START, "outbrains/output")
+# def test_gpu_benchmark(tmpdir):
+#     input_path = os.path.join(DATA_START, "outbrains/input")
+#     output_path = os.path.join(DATA_START, "outbrains/output")
 
-    notebook_path = os.path.join(dirname(TEST_PATH), "examples", "gpu_benchmark.ipynb")
-    _run_notebook(tmpdir, notebook_path, input_path, output_path, gpu_id=0, batch_size=100000)
+#     notebook_path = os.path.join(dirname(TEST_PATH), "examples", "gpu_benchmark.ipynb")
+#     _run_notebook(tmpdir, notebook_path, input_path, output_path, gpu_id=0, batch_size=100000)
 
 
 def _run_notebook(
@@ -161,18 +162,11 @@ def _run_notebook(
         shutil.rmtree(output_path)
     return output
 
-
-def create_bench_result(name, arg_tuple_list, result, unit):
-    return BenchmarkResult(funcName=name,
-                           argNameValuePairs=arg_tuple_list,
-                           unit=unit,
-                           result=result)
-
-
-        
-class bench_fastai():
-    def __init__(self, target_id):
+class BenchFastAI():
+    def __init__(self, target_id, val=1, split=None):
         self.name = f"{target_id}_fastai"
+        self.val = val
+        self.split = split
     
     def bres_loss(self, epoch, loss, l_type="train"):
         return create_bench_result(f"{self.name}_{l_type}_loss",
@@ -185,11 +179,23 @@ class bench_fastai():
         return create_bench_result(f"{self.name}_exp_rmspe",
                                    [("epoch", epoch)],
                                    rmspe,
-                                   "percent")  
-
+                                   "percent")
     
-    def bres_time(self, epoch, r_time):
-        x = time.strptime(r_time.split(',')[0],'%M:%S')
+    def bres_acc(self, epoch, acc):
+        return create_bench_result(f"{self.name}_exp_rmspe",
+                                   [("epoch", epoch)],
+                                   acc,
+                                   "percent")
+
+    def bres_roc_auc(self, epoch, acc):
+        return create_bench_result(f"{self.name}_exp_rmspe",
+                                   [("epoch", epoch)],
+                                   acc,
+                                   "percent")
+    
+    
+    def bres_time(self, epoch, r_time, time_format='%M:%S'):
+        x = time.strptime(r_time.split(',')[0],time_format)
         r_time = datetime.timedelta(
                     hours=x.tm_hour,
                     minutes=x.tm_min,
@@ -200,7 +206,34 @@ class bench_fastai():
                                    r_time,
                                    "seconds")      
     
-    
+    def get_epochs(self, output):
+        epochs = []
+        for line in output:
+            split_line = line.split(self.split) if self.split else line.split()
+            if len(split_line) == self.val and is_number(split_line[0]):
+                # epoch line, detected based on if 1st character is a number
+                post_evts = self.get_epoch(line)
+                epochs.append(post_evts)
+        return epochs
+
+class CriteoBenchFastAI(BenchFastAI):
+    def __init__(self, val=5, split=None):
+        super().__init__("Criteo", val=val, split=split)
+        
+    def get_epoch(self, line):
+        epoch, t_loss, v_loss, acc, o_time = line.split()
+        t_loss = self.bres_loss(epoch, float(t_loss))
+        v_loss = self.bres_loss(epoch, float(v_loss), l_type="valid")
+        acc = self.bres_acc(epoch, float(acc))
+#         roc = self.bres_roc_auc(epoch, float(roc_auc))
+        o_time = self.bres_time(epoch, o_time)
+        return [t_loss, v_loss, acc, o_time]
+
+
+class RossBenchFastAI(BenchFastAI):
+    def __init__(self, val=5, split=None):
+        super().__init__("Rossmann", val=val, split=split)
+        
     def get_epoch(self, line):
         epoch, t_loss, v_loss, exp_rmspe, o_time = line.split()
         t_loss = self.bres_loss(epoch, float(t_loss))
@@ -209,16 +242,8 @@ class bench_fastai():
         o_time = self.bres_time(epoch, o_time)
         return [t_loss, v_loss, exp_rmspe, o_time]
     
-    
-    def get_epochs(self, output):
-        epochs = []
-        for line in output:
-            split_line = line.split()
-            if len(split_line) > 1 and is_number(split_line[0]):
-                # epoch line, detected based on if 1st character is a number
-                post_evts = self.get_epoch(line)
-                epochs.append(post_evts)
-        return epochs
+        
+
 
 def is_number(str_to_num):
     try:
@@ -227,5 +252,16 @@ def is_number(str_to_num):
     except ValueError:
         return False
                 
+def send_results(db, bench_info, results_list):
+    for results in results_list:
+        if isinstance(results, list):
+            for result in results:
+                db.addResult(bench_info, result)
+        else:
+            db.addResult(bench_info, results)
 
-    
+def create_bench_result(name, arg_tuple_list, result, unit):
+    return BenchmarkResult(funcName=name,
+                           argNameValuePairs=arg_tuple_list,
+                           unit=unit,
+                           result=result)
