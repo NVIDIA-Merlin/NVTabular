5b47ef2b372cece0bc84504d069bdd35a4af1553
Alberto Alvarez <albertoa@nvidia.com>
2020-12-16 17:14:48 +0000

    Basic Function Working + config files

--- tools/inspect_dataset.py
+++ tools/inspect_dataset.py
@@ -18,11 +18,32 @@ import argparse
 import fsspec
 import json
 import nvtabular as nvt
+import numpy as np
+from functools import singledispatch
 
+from dask.distributed import Client
+from dask_cuda import LocalCUDACluster
+
+class NpEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, np.integer):
+            return int(obj)
+        elif isinstance(obj, np.floating):
+            return float(obj)
+        elif isinstance(obj, np.ndarray):
+            return obj.tolist()
+        else:
+            return super(NpEncoder, self).default(obj)
 
 def parse_args():
     parser = argparse.ArgumentParser(description=("Dataset Inspect Tool"))
-
+    # Config file
+    parser.add_argument(
+        "-c",
+        "--config_file",
+        type=str,
+        help='Dataset columns type (Required)',
+    )
     # Dataset path
     parser.add_argument(
         "-d",
@@ -31,7 +52,6 @@ def parse_args():
         type=str,
         help='Input dataset path (Required)',
     )
-    
     # Dataset format
     parser.add_argument(
         "-f",
@@ -41,7 +61,6 @@ def parse_args():
         type=str,
         help="Dataset format (Default 'parquet')",
     )
-
     # Output file name
     parser.add_argument(
         "-o",
@@ -50,31 +69,76 @@ def parse_args():
         type=str,
         help="Output file name (Default 'dataset_info.json')",
     )
-
     args = parser.parse_args()
     return args
 
+def get_all_stats(ddf, col, data):
+    data[col] = {}
+    # Get dtype
+    data[col]['dtype'] = str(ddf[col].dtype) 
+    if data[col]['dtype'] == "object":
+        data[col]['dtype'] = "string"
+    data[col]['nans_%'] = 100 * (1 - ddf[col].count().compute() / len(ddf[col]))
+
+def get_cats_stats(ddf, col, data):
+    data[col]['cardinality'] = ddf[col].nunique().compute()
+    
+def get_conts_stats(ddf, col, data):
+    data[col]['min'] = ddf[col].min().compute()
+    data[col]['max'] = ddf[col].max().compute()
+    data[col]['mean'] = ddf[col].mean().compute()
+    data[col]['std'] = ddf[col].std().compute()
+
+def get_labels_stats(ddf, col, data):
+    data[col]['cardinality'] = ddf[col].nunique().compute()
+
 def main(args):
+    # Get dataset columns
+    with fsspec.open(args.config_file) as f:
+        config = json.load(f)
+    cats = config['cats']
+    conts = config['conts']
+    labels = config['labels']
+    columns = cats+conts+labels
+
     # Get dataset
     dataset = nvt.Dataset(args.data_path, engine=args.format)
     ddf = dataset.to_ddf()
-    
+    print(ddf.dtypes)
+
+    # Create Dask Cluster
+    cluster = LocalCUDACluster()
+    client = Client(cluster)
+    print(client)
+
     # Dictionary to store collected information
     data = {}
-    #data['num_rows'] = dataset.num_rows
+    # Store general info
+    # data['num_rows'] = dataset.num_rows
+    data['cats'] = cats
+    data['conts'] = conts
+    data['labels'] = labels
 
-    # Get columns and dtypes
-    for col in ddf.columns:
-        data[col] = {}
-        data[col]['dtype'] = str(ddf[col].dtype)
-        data[col]['min'] = ddf[col].min().compute()
-        data[col]['max'] = ddf[col].max().compute()
+    # Get continuous columnd stats
+    for col in cats:
+        get_all_stats(ddf, col, data)
+        get_cats_stats(ddf, col, data)
+
+    # Get continuous columnd stats
+    for col in conts:
+        get_all_stats(ddf, col, data)
+        get_conts_stats(ddf, col, data)
+
+    # Get labels columns stats
+    for col in conts:
+        get_all_stats(ddf, col, data)
+        get_labels_stats(ddf, col, data)
     
     print(data)
 
     # Write json file
     with fsspec.open(args.output_file, 'w') as outfile:
-        json.dump(data, outfile)
+        json.dump(data, outfile, cls=NpEncoder)
 
 if __name__ == "__main__":
     main(parse_args())
