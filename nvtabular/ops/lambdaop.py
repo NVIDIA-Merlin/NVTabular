0eb78be8af012344244be669c9cefd5be24d6af8
rnyak <ronayak@hotmail.com>
2020-12-17 15:35:47 -0800

    edit docstrings

--- nvtabular/ops/lambdaop.py
+++ nvtabular/ops/lambdaop.py
@@ -27,52 +27,32 @@ class LambdaOp(Operator):
 
     Example usage 1::
 
-        # Initialize the workflow
-        proc = nvt.Workflow(
-            cat_names=CATEGORICAL_COLUMNS,
-            cont_names=CONTINUOUS_COLUMNS,
-            label_name=LABEL_COLUMNS
-        )
-
-        # Add LamdaOp to the workflow and
-        # specify an op_name
-        # define a custom function e.g. extract first 5 character from string
-        proc.add_feature(
-            LambdaOp(
-                op_name='first5char', #op_name - merged with column name
-                f=lambda col: col.str.slice(0,5), # custom function
-                columns=['cat1', 'cat2', 'cat3'], # columns, f is applied to
-                replace=False # if columns will be replaced
-            )
-        )
+        # Define a ColumnGroup that LamdaOp will apply to
+        # then define a custom function, e.g. extract first 5 character from a string
+        lambda_feature = ColumnGroup(["col1"])
+        new_lambda_feature = lambda_feature >> (lambda col: col.str.slice(0, 5))
+        processor = nvtabular.Workflow(new_lambda_feature + 'label')
 
     Example usage 2::
 
-        # Add LambdaOp to the workflow and
-        # specify an op_name
-        # define a custom function e.g. calculate probability
-        # for different events.
-        proc.add_feature(
-            LambdaOp(
-                op_name='cond_prob', #op_name - merged with column name
-                f=lambda col, gdf: col.astype(np.float32) / gdf['total_events'], # custom function
-                columns=['event1', 'event2', 'event3'], # columns, f is applied to
-                replace=False # if columns will be replaced
-            )
-        )
+        # define a custom function e.g. calculate probability for different events.
+        # Rename the each new feature column name.
+        lambda_features = ColumnGroup(['event1', 'event2', 'event3']), # columns, f is applied to
+        def cond_prob(col, gdf):
+            col = col.astype(np.float32)
+            col = col / gdf['total_events']
+            return col
+        new_lambda_features = lambda_features >> LambdaOp(cond_prob, dependency=["total_events"]) \
+>> Rename(postfix="_cond")
+        processor = nvtabular.Workflow(new_lambda_features + 'label')
 
     Parameters
     -----------
-    op_name : str:
-        name of the operator column. It is used as a post_fix for the modified column names
-        (if replace=False).
     f : callable
         Defines a function that takes a cudf.Series and an optional cudf.DataFrame as input,
         and returns a new Series as the output.
-    columns :
-        Columns to target for this op. If None, this operator will target all columns.
-    replace : bool, default True
-        Whether to replace existing columns or create new ones.
+    dependency : list, default None
+        Whether to provide a dependency column or not.
     """
 
     def __init__(self, f, dependency=None):
