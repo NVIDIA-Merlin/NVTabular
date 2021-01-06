69dbe1f6d7063460a3245215399a3a112f963f02
Ben Frederickson <github@benfrederickson.com>
2021-01-06 10:00:17 -0800

    Fix bad merge

--- nvtabular/ops/logop.py
+++ nvtabular/ops/logop.py
@@ -39,4 +39,4 @@ class LogOp(Operator):
 
     @annotate("LogOp_op", color="darkgreen", domain="nvt_python")
     def transform(self, columns, gdf: cudf.DataFrame):
-        return (gdf[cont_names].astype(np.float32) + 1).log()
+        return (gdf[columns].astype(np.float32) + 1).log()
