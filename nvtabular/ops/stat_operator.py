c80559283c91a68b7e855bd14027d1ee6cf6f884
Ben Frederickson <github@benfrederickson.com>
2021-01-28 09:43:04 -0800

    Workflow serialization (#536)
    The existing load_stats/save_stats code only saved the calculated statistics for a workflow, but
    didn't save all the operators that were used as well as the processing graph. This makes it
    difficult to recreate the workflow exactly for cases like inferences
    
    This PR adds support for saving and loading the entire workflow object. Since the operators
    being applied could use lambdas or be custom operators written in jupyter notebooks - we're
    using cloudpickle for serialization (cloudpickle is also used by dask).

--- nvtabular/ops/stat_operator.py
+++ nvtabular/ops/stat_operator.py
@@ -45,16 +45,15 @@ class StatOperator(Operator):
             """Follow-up operations to convert dask statistics in to member variables"""
         )
 
-    def save(self):
-        """Returns a json-able representation of the statistics for this object. This
-        is usually called by the workflow rather than diretly"""
-        raise NotImplementedError("save isn't implemented for this op!")
-
-    def load(self, data):
-        """Loads statistics from a json-able blob of data. This is usually called
-        by the workflow rather than called directly"""
-        raise NotImplementedError("load isn't implemented for this op!")
-
     def clear(self):
         """ zero and reinitialize all relevant statistical properties"""
         raise NotImplementedError("clear isn't implemented for this op!")
+
+    def set_storage_path(self, path, copy=False):
+        """Certain stat operators need external storage - for instance Categorify writes out
+        parquet files containing the categorical mapping. When we save the operator, we
+        also want to save these files as part of the bundle. Implementing this method
+        lets statoperators bundle their dependant files into the new path that we're writing
+        out (note that this could happen after the operator is created)
+        """
+        pass
