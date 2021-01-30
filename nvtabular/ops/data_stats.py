80f267f8ab98a551d6da600d4c19fdcbe4402326
Ben Frederickson <github@benfrederickson.com>
2021-01-29 17:20:30 -0800

    Fix unittests

--- nvtabular/ops/data_stats.py
+++ nvtabular/ops/data_stats.py
@@ -13,6 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+import cudf
 import dask_cudf
 import numpy as np
 from nvtx import annotate
@@ -28,6 +29,9 @@ class DataStats(StatOperator):
         self.col_types = []
         self.col_dtypes = []
         self.output = {}
+
+    def transform(self, columns: ColumnNames, gdf: cudf.DataFrame) -> cudf.DataFrame:
+        return gdf
 
     @annotate("DataStats_fit", color="green", domain="nvt_python")
     def fit(self, columns: ColumnNames, ddf: dask_cudf.DataFrame):
