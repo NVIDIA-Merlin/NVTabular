1ad3cda41983e02eb282bd227d59673951cdaeb4
rjzamora <rzamora217@gmail.com>
2021-03-01 15:20:22 -0800

    add dask-bug workaround for s3 test

--- tests/unit/test_s3.py
+++ tests/unit/test_s3.py
@@ -15,11 +15,16 @@
 #
 
 import os
+import shlex
+import subprocess
+import time
+from contextlib import contextmanager
+from io import BytesIO
 
 import cudf
-import fsspec
 import pytest
 from cudf.tests.utils import assert_eq
+from dask.dataframe.io.parquet.core import create_metadata_file
 
 import nvtabular as nvt
 from nvtabular import ops as ops
@@ -28,50 +33,151 @@ from tests.conftest import mycols_csv, mycols_pq
 boto3 = pytest.importorskip("boto3")
 s3fs = pytest.importorskip("s3fs")
 moto = pytest.importorskip("moto")
+requests = pytest.importorskip("requests")
 
 
-@pytest.fixture(scope="function")
-def aws_credentials():
-    """Mocked AWS Credentials for moto."""
-    os.environ["AWS_ACCESS_KEY_ID"] = "testing"
-    os.environ["AWS_SECRET_ACCESS_KEY"] = "testing"
-    os.environ["AWS_SECURITY_TOKEN"] = "testing"
-    os.environ["AWS_SESSION_TOKEN"] = "testing"
+@contextmanager
+def ensure_safe_environment_variables():
+    """
+    Get a context manager to safely set environment variables
+    All changes will be undone on close, hence environment variables set
+    within this contextmanager will neither persist nor change global state.
+
+    This function was copied from https://github.com/rapidsai/cudf
+    (see cudf/python/dask_cudf/dask_cudf/io/tests/test_s3.py)
+    """
+    saved_environ = dict(os.environ)
+    try:
+        yield
+    finally:
+        os.environ.clear()
+        os.environ.update(saved_environ)
 
 
-@pytest.fixture(scope="function")
-def s3(aws_credentials):
-    with moto.mock_s3():
-        yield boto3.client("s3", region_name="us-east-1")
+@pytest.fixture(scope="session")
+def s3_base(worker_id):
+    """
+    Fixture to set up moto server in separate process
+
+    This fixture was copied from https://github.com/rapidsai/cudf
+    (see cudf/python/dask_cudf/dask_cudf/io/tests/test_s3.py)
+    """
+    with ensure_safe_environment_variables():
+        # Fake aws credentials exported to prevent botocore looking for
+        # system aws credentials, https://github.com/spulec/moto/issues/1793
+        os.environ.setdefault("AWS_ACCESS_KEY_ID", "foobar_key")
+        os.environ.setdefault("AWS_SECRET_ACCESS_KEY", "foobar_secret")
+
+        # Launching moto in server mode, i.e., as a separate process
+        # with an S3 endpoint on localhost
+
+        endpoint_port = 5000 if worker_id == "master" else 5550 + int(worker_id.lstrip("gw"))
+        endpoint_uri = f"http://127.0.0.1:{endpoint_port}/"
+
+        proc = subprocess.Popen(
+            shlex.split(f"moto_server s3 -p {endpoint_port}"),
+        )
+
+        timeout = 5
+        while timeout > 0:
+            try:
+                # OK to go once server is accepting connections
+                r = requests.get(endpoint_uri)
+                if r.ok:
+                    break
+            except Exception:
+                pass
+            timeout -= 0.1
+            time.sleep(0.1)
+        yield endpoint_uri
+
+        proc.terminate()
+        proc.wait()
+
+
+@pytest.fixture()
+def s3so(worker_id):
+    """
+    Returns s3 storage options to pass to fsspec
+
+    This fixture was copied from https://github.com/rapidsai/cudf
+    (see cudf/python/dask_cudf/dask_cudf/io/tests/test_s3.py)
+    """
+    endpoint_port = 5000 if worker_id == "master" else 5550 + int(worker_id.lstrip("gw"))
+    endpoint_uri = f"http://127.0.0.1:{endpoint_port}/"
+
+    return {"client_kwargs": {"endpoint_url": endpoint_uri}}
+
+
+@contextmanager
+def s3_context(s3_base, bucket, files=None):
+    """
+    This function was copied from https://github.com/rapidsai/cudf
+    (see cudf/python/dask_cudf/dask_cudf/io/tests/test_s3.py)
+    """
+    if files is None:
+        files = {}
+    with ensure_safe_environment_variables():
+        client = boto3.client("s3", endpoint_url=s3_base)
+        client.create_bucket(Bucket=bucket, ACL="public-read-write")
+        for f, data in files.items():
+            client.put_object(Bucket=bucket, Key=f, Body=data)
+
+        yield s3fs.S3FileSystem(client_kwargs={"endpoint_url": s3_base})
+
+        for f, data in files.items():
+            try:
+                client.delete_object(Bucket=bucket, Key=f)
+            except Exception:
+                pass
 
 
 @pytest.mark.parametrize("engine", ["parquet", "csv"])
-def test_s3_dataset(s3, paths, engine, df):
-    # create a mocked out bucket here
-    bucket = "testbucket"
-    s3.create_bucket(Bucket=bucket)
+def test_s3_dataset(s3_base, s3so, paths, datasets, engine, df):
 
-    s3_paths = []
-    for path in paths:
-        s3_path = f"s3://{bucket}/{path}"
-        with fsspec.open(s3_path, "wb") as f:
-            f.write(open(path, "rb").read())
-        s3_paths.append(s3_path)
+    # Copy files to mock s3 bucket
+    files = {}
+    for i, path in enumerate(paths):
+        with open(path, "rb") as f:
+            fbytes = f.read()
+        fn = path.split(os.path.sep)[-1]
+        files[fn] = BytesIO()
+        files[fn].write(fbytes)
+        files[fn].seek(0)
 
-    # create a basic s3 dataset
-    dataset = nvt.Dataset(s3_paths)
+    if engine == "parquet":
+        # Workaround for nvt#539. In order to avoid the
+        # bug in Dask's `create_metadata_file`, we need
+        # to manually generate a "_metadata" file here.
+        # This can be removed after dask#7295 is merged
+        # (see https://github.com/dask/dask/pull/7295)
+        fn = "_metadata"
+        files[fn] = BytesIO()
+        meta = create_metadata_file(
+            paths,
+            engine="pyarrow",
+            out_dir=False,
+        )
+        meta.write_metadata_file(files[fn])
+        files[fn].seek(0)
 
-    # make sure the iteration API works
-    columns = mycols_pq if engine == "parquet" else mycols_csv
-    gdf = cudf.concat(list(dataset.to_iter()))[columns]
-    assert_eq(gdf.reset_index(drop=True), df.reset_index(drop=True))
+    with s3_context(s3_base=s3_base, bucket=engine, files=files):
 
-    cat_names = ["name-cat", "name-string"] if engine == "parquet" else ["name-string"]
-    cont_names = ["x", "y", "id"]
-    label_name = ["label"]
+        # Create nvt.Dataset from mock s3 paths
+        url = f"s3://{engine}" if engine == "parquet" else f"s3://{engine}/*"
+        dataset = nvt.Dataset(url, engine=engine, storage_options=s3so)
 
-    conts = cont_names >> ops.FillMissing() >> ops.Clip(min_value=0) >> ops.LogOp()
-    cats = cat_names >> ops.Categorify(cat_cache="host")
+        # Check that the iteration API works
+        columns = mycols_pq if engine == "parquet" else mycols_csv
+        gdf = cudf.concat(list(dataset.to_iter()))[columns]
+        assert_eq(gdf.reset_index(drop=True), df.reset_index(drop=True))
 
-    processor = nvt.Workflow(conts + cats + label_name)
-    processor.fit(dataset)
+        cat_names = ["name-cat", "name-string"] if engine == "parquet" else ["name-string"]
+        cont_names = ["x", "y", "id"]
+        label_name = ["label"]
+
+        conts = cont_names >> ops.FillMissing() >> ops.Clip(min_value=0) >> ops.LogOp()
+        cats = cat_names >> ops.Categorify(cat_cache="host")
+
+        processor = nvt.Workflow(conts + cats + label_name)
+        processor.fit(dataset)
