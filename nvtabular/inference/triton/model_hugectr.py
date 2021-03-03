abaa29047c47bcdee78f1f5b16ce3ae320b11dfc
Onur Yilmaz <oyilmaz@nvidia.com>
2021-03-03 17:31:21 -0500

    Fix len error

--- nvtabular/inference/triton/model_hugectr.py
+++ nvtabular/inference/triton/model_hugectr.py
@@ -94,8 +94,8 @@ class TritonPythonModel:
                 output_tensors.append(Tensor("CATCOLUMN", np.array([])))
 
             len_cats_np = cats_np.shape[1]
-            row_index = np.arange(len(len_cats_np) + 1, dtype=np.int32).reshape(
-                1, len(len_cats_np) + 1
+            row_index = np.arange(len_cats_np + 1, dtype=np.int32).reshape(
+                1, len_cats_np + 1
             )
             output_tensors.append(Tensor("ROWINDEX", row_index))
 
