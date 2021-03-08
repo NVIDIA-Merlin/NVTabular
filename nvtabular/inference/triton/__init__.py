32c3de9d9bd44fb011d9bd4d7e3dd25d875ddb25
Onur Yilmaz <35306097+oyilmaz-nvidia@users.noreply.github.com>
2021-03-08 18:18:29 -0500

    Export NVTabular + HugeCTR Ensemble Config (#634)

--- nvtabular/inference/triton/__init__.py
+++ nvtabular/inference/triton/__init__.py
@@ -61,7 +61,7 @@ def export_tensorflow_ensemble(model, workflow, name, model_path, label_columns,
 
     # generate the nvtabular triton model
     preprocessing_path = os.path.join(model_path, name + "_nvt")
-    nvt_config = generate_triton_model(workflow, name + "_nvt", preprocessing_path)
+    nvt_config = generate_nvtabular_model(workflow, name + "_nvt", preprocessing_path)
 
     # generate the TF saved model
     tf_path = os.path.join(model_path, name + "_tf")
@@ -73,83 +73,93 @@ def export_tensorflow_ensemble(model, workflow, name, model_path, label_columns,
     ensemble_path = os.path.join(model_path, name)
     os.makedirs(ensemble_path, exist_ok=True)
     os.makedirs(os.path.join(ensemble_path, str(version)), exist_ok=True)
-    _generate_ensemble_config(model, workflow, name, ensemble_path, nvt_config, tf_config)
+    _generate_ensemble_config(name, ensemble_path, nvt_config, tf_config)
 
 
-def _remove_columns(workflow, to_remove):
-    workflow = copy.deepcopy(workflow)
+def export_hugectr_ensemble(
+    workflow,
+    hugectr_model_path,
+    hugectr_params,
+    name,
+    output_path,
+    label_columns,
+    version=1,
+    cats=None,
+    conts=None,
+    max_batch_size=None,
+):
+    """Creates an ensemble hugectr server model, with the first model being a nvtabular
+    preprocessing, and the second by a hugectr savedmodel
 
-    workflow.column_group = _remove_columns_from_column_group(workflow.column_group, to_remove)
+    Parameters
+    ----------
+    workflow:
+        The nvtabular workflow used in preprocessing
+    hugectr_model_path:
+        The path of the trained model files
+    hugectr_params:
+        HugeCTR specific parameters
+    name:
+        The base name of the various triton models
+    output_path:
+        The path where the models will be served
+    label_columns:
+        Labels in the dataset (will be removed from the workflow)
+    version:
+        The version of the model
+    cats:
+        Names of the categorical columns
+    conts:
+        Names of the continous columns
+    max_batch_size:
+        Max batch size that Triton can receive
 
-    for label in to_remove:
-        if label in workflow.input_dtypes:
-            del workflow.input_dtypes[label]
+    """
 
-        if label in workflow.output_dtypes:
-            del workflow.output_dtypes[label]
+    workflow = _remove_columns(workflow, label_columns)
 
-    return workflow
-
-
-def _remove_columns_from_column_group(cg, to_remove):
-    cg.columns = [col for col in cg.columns if col not in to_remove]
-    parents = [_remove_columns_from_column_group(parent, to_remove) for parent in cg.parents]
-    cg.parents = [p for p in parents if p.columns]
-    return cg
-
-
-def _generate_ensemble_config(model, workflow, name, output_path, nvt_config, tf_config):
-    config = model_config.ModelConfig(name=name, platform="ensemble")
-    config.input.extend(nvt_config.input)
-    config.output.extend(tf_config.output)
-
-    nvt_step = model_config.ModelEnsembling.Step(model_name=name + "_nvt", model_version=-1)
-    for input_col in nvt_config.input:
-        nvt_step.input_map[input_col.name] = input_col.name
-    for output_col in nvt_config.output:
-        nvt_step.output_map[output_col.name] = output_col.name + "_nvt"
-
-    tf_step = model_config.ModelEnsembling.Step(model_name=name + "_tf", model_version=-1)
-    for input_col in tf_config.input:
-        tf_step.input_map[input_col.name] = input_col.name + "_nvt"
-    for output_col in tf_config.output:
-        tf_step.output_map[output_col.name] = output_col.name
-
-    config.ensemble_scheduling.step.append(nvt_step)
-    config.ensemble_scheduling.step.append(tf_step)
-
-    with open(os.path.join(output_path, "config.pbtxt"), "w") as o:
-        text_format.PrintMessage(config, o)
-    return config
-
-
-def _generate_tensorflow_config(model, name, output_path):
-    """given a workflow generates the trton modelconfig proto object describing the inputs
-    and outputs to that workflow"""
-    config = model_config.ModelConfig(
-        name=name, backend="tensorflow", platform="tensorflow_savedmodel"
+    # generate the nvtabular triton model
+    preprocessing_path = os.path.join(output_path, name + "_nvt")
+    nvt_config = generate_nvtabular_model(
+        workflow=workflow,
+        name=name + "_nvt",
+        output_path=preprocessing_path,
+        version=version,
+        output_model="hugectr",
+        cats=cats,
+        conts=conts,
+        max_batch_size=max_batch_size,
     )
 
-    for col in model.inputs:
-        config.input.append(
-            model_config.ModelInput(
-                name=col.name, data_type=_convert_dtype(col.dtype), dims=[-1, 1]
-            )
-        )
+    hugectr_params["label_dim"] = len(label_columns)
+    if conts is None:
+        hugectr_params["des_feature_num"] = 0
+    else:
+        hugectr_params["des_feature_num"] = len(conts)
 
-    for col in model.outputs:
-        config.output.append(
-            model_config.ModelOutput(
-                name=col.name.split("/")[0], data_type=_convert_dtype(col.dtype), dims=[-1, 1]
-            )
-        )
+    if cats is None:
+        hugectr_params["cat_feature_num"] = 0
+    else:
+        hugectr_params["cat_feature_num"] = len(cats)
 
-    with open(os.path.join(output_path, "config.pbtxt"), "w") as o:
-        text_format.PrintMessage(config, o)
-    return config
+    # generate the HugeCTR saved model
+    hugectr_config = generate_hugectr_model(
+        trained_model_path=hugectr_model_path,
+        hugectr_params=hugectr_params,
+        name=name,
+        output_path=output_path,
+        version=version,
+        max_batch_size=max_batch_size,
+    )
+
+    # generate the triton ensemble
+    ensemble_path = os.path.join(output_path, name + "_ens")
+    os.makedirs(ensemble_path, exist_ok=True)
+    os.makedirs(os.path.join(ensemble_path, str(version)), exist_ok=True)
+    _generate_ensemble_config(name, ensemble_path, nvt_config, hugectr_config, "_ens")
 
 
-def generate_triton_model(
+def generate_nvtabular_model(
     workflow,
     name,
     output_path,
@@ -162,7 +172,7 @@ def generate_triton_model(
     """ converts a workflow to a triton mode """
 
     workflow.save(os.path.join(output_path, str(version), "workflow"))
-    config = _generate_model_config(
+    config = _generate_nvtabular_config(
         workflow, name, output_path, output_model, max_batch_size, cats, conts
     )
 
@@ -176,6 +186,31 @@ def generate_triton_model(
         copyfile(
             os.path.join(os.path.dirname(__file__), "model.py"),
             os.path.join(output_path, str(version), "model.py"),
+        )
+
+    return config
+
+
+def generate_hugectr_model(
+    trained_model_path,
+    hugectr_params,
+    name,
+    output_path,
+    version=1,
+    max_batch_size=None,
+):
+    """ converts a trained HugeCTR model to a triton mode """
+
+    out_path = os.path.join(output_path, name)
+    os.makedirs(os.path.join(output_path, name), exist_ok=True)
+    out_path_version = os.path.join(out_path, str(version))
+    os.makedirs(out_path_version, exist_ok=True)
+
+    config = _generate_hugectr_config(name, out_path, hugectr_params, max_batch_size=max_batch_size)
+    for fname in os.listdir(trained_model_path):
+        copyfile(
+            os.path.join(trained_model_path, fname),
+            os.path.join(out_path_version, fname),
         )
 
     return config
@@ -212,7 +247,7 @@ def convert_triton_output_to_df(columns, response):
     return cudf.DataFrame({col: response.as_numpy(col) for col in columns})
 
 
-def _generate_model_config(
+def _generate_nvtabular_config(
     workflow, name, output_path, output_model=None, max_batch_size=None, cats=None, conts=None
 ):
     """given a workflow generates the trton modelconfig proto object describing the inputs
@@ -248,6 +283,152 @@ def _generate_model_config(
     with open(os.path.join(output_path, "config.pbtxt"), "w") as o:
         text_format.PrintMessage(config, o)
     return config
+
+
+def _generate_ensemble_config(name, output_path, nvt_config, nn_config, name_ext=""):
+    config = model_config.ModelConfig(
+        name=name + name_ext, platform="ensemble", max_batch_size=nvt_config.max_batch_size
+    )
+    config.input.extend(nvt_config.input)
+    config.output.extend(nn_config.output)
+
+    nvt_step = model_config.ModelEnsembling.Step(model_name=nvt_config.name, model_version=-1)
+    for input_col in nvt_config.input:
+        nvt_step.input_map[input_col.name] = input_col.name
+    for output_col in nvt_config.output:
+        nvt_step.output_map[output_col.name] = output_col.name + "_nvt"
+
+    tf_step = model_config.ModelEnsembling.Step(model_name=nn_config.name, model_version=-1)
+    for input_col in nn_config.input:
+        tf_step.input_map[input_col.name] = input_col.name + "_nvt"
+    for output_col in nn_config.output:
+        tf_step.output_map[output_col.name] = output_col.name
+
+    config.ensemble_scheduling.step.append(nvt_step)
+    config.ensemble_scheduling.step.append(tf_step)
+
+    with open(os.path.join(output_path, "config.pbtxt"), "w") as o:
+        text_format.PrintMessage(config, o)
+    return config
+
+
+def _generate_tensorflow_config(model, name, output_path):
+    """given a workflow generates the trton modelconfig proto object describing the inputs
+    and outputs to that workflow"""
+    config = model_config.ModelConfig(
+        name=name, backend="tensorflow", platform="tensorflow_savedmodel"
+    )
+
+    for col in model.inputs:
+        config.input.append(
+            model_config.ModelInput(
+                name=col.name, data_type=_convert_dtype(col.dtype), dims=[-1, 1]
+            )
+        )
+
+    for col in model.outputs:
+        config.output.append(
+            model_config.ModelOutput(
+                name=col.name.split("/")[0], data_type=_convert_dtype(col.dtype), dims=[-1, 1]
+            )
+        )
+
+    with open(os.path.join(output_path, "config.pbtxt"), "w") as o:
+        text_format.PrintMessage(config, o)
+    return config
+
+
+def _generate_hugectr_config(name, output_path, hugectr_params, max_batch_size=None):
+    config = model_config.ModelConfig(name=name, backend="hugectr", max_batch_size=max_batch_size)
+
+    config.input.append(
+        model_config.ModelInput(name="DES", data_type=model_config.TYPE_FP32, dims=[-1])
+    )
+
+    config.input.append(
+        model_config.ModelInput(name="CATCOLUMN", data_type=model_config.TYPE_INT64, dims=[-1])
+    )
+
+    config.input.append(
+        model_config.ModelInput(name="ROWINDEX", data_type=model_config.TYPE_INT32, dims=[-1])
+    )
+
+    for i in range(hugectr_params["n_outputs"]):
+        config.output.append(
+            model_config.ModelOutput(
+                name="OUTPUT" + str(i), data_type=model_config.TYPE_FP32, dims=[-1]
+            )
+        )
+
+    config.instance_group.append(model_config.ModelInstanceGroup(gpus=[0], count=1, kind=1))
+
+    config_hugectr = model_config.ModelParameter(string_value=hugectr_params["config"])
+    config.parameters["config"].CopyFrom(config_hugectr)
+
+    gpucache_val = hugectr_params.get("gpucache", "true")
+
+    gpucache = model_config.ModelParameter(string_value=gpucache_val)
+    config.parameters["gpucache"].CopyFrom(gpucache)
+
+    gpucacheper_val = str(hugectr_params.get("gpucacheper_val", "0.5"))
+
+    gpucacheper = model_config.ModelParameter(string_value=gpucacheper_val)
+    config.parameters["gpucacheper"].CopyFrom(gpucacheper)
+
+    label_dim = model_config.ModelParameter(string_value=str(hugectr_params["label_dim"]))
+    config.parameters["label_dim"].CopyFrom(label_dim)
+
+    slots = model_config.ModelParameter(string_value=str(hugectr_params["slots"]))
+    config.parameters["slots"].CopyFrom(slots)
+
+    des_feature_num = model_config.ModelParameter(
+        string_value=str(hugectr_params["des_feature_num"])
+    )
+    config.parameters["des_feature_num"].CopyFrom(des_feature_num)
+
+    cat_feature_num = model_config.ModelParameter(
+        string_value=str(hugectr_params["cat_feature_num"])
+    )
+    config.parameters["cat_feature_num"].CopyFrom(cat_feature_num)
+
+    max_nnz = model_config.ModelParameter(string_value=str(hugectr_params["max_nnz"]))
+    config.parameters["max_nnz"].CopyFrom(max_nnz)
+
+    embedding_vector_size = model_config.ModelParameter(
+        string_value=str(hugectr_params["embedding_vector_size"])
+    )
+    config.parameters["embedding_vector_size"].CopyFrom(embedding_vector_size)
+
+    embeddingkey_long_type_val = hugectr_params.get("embeddingkey_long_type", "true")
+
+    embeddingkey_long_type = model_config.ModelParameter(string_value=embeddingkey_long_type_val)
+    config.parameters["embeddingkey_long_type"].CopyFrom(embeddingkey_long_type)
+
+    with open(os.path.join(output_path, "config.pbtxt"), "w") as o:
+        text_format.PrintMessage(config, o)
+    return config
+
+
+def _remove_columns(workflow, to_remove):
+    workflow = copy.deepcopy(workflow)
+
+    workflow.column_group = _remove_columns_from_column_group(workflow.column_group, to_remove)
+
+    for label in to_remove:
+        if label in workflow.input_dtypes:
+            del workflow.input_dtypes[label]
+
+        if label in workflow.output_dtypes:
+            del workflow.output_dtypes[label]
+
+    return workflow
+
+
+def _remove_columns_from_column_group(cg, to_remove):
+    cg.columns = [col for col in cg.columns if col not in to_remove]
+    parents = [_remove_columns_from_column_group(parent, to_remove) for parent in cg.parents]
+    cg.parents = [p for p in parents if p.columns]
+    return cg
 
 
 def _add_model_param(column, dtype, paramclass, params):
