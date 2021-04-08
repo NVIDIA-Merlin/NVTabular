c351cdfa71a6e519f876150db88130e58cf969a4
Karl Higley <kmhigley@gmail.com>
2021-04-08 15:27:46 -0400

    Add a dataloader backend test for random seeding behavior during shuffle

--- nvtabular/loader/backend.py
+++ nvtabular/loader/backend.py
@@ -245,8 +245,17 @@ class DataLoader:
 
     def _generate_local_seed(self):
         random_state = cp.random.get_random_state()
-        local_seed = random_state.tomaxint(size=self.global_size)[self.global_rank]
-        cp.random.seed(local_seed)
+        seeds = random_state.tomaxint(size=self.global_size)
+        local_seed = seeds[self.global_rank]
+        cp.random.seed(local_seed.get())
+
+    def _shuffle_indices(self):
+        self._generate_local_seed()
+        if self.seed_fn:
+            new_seed = self.seed_fn()
+            cp.random.seed(new_seed)
+        cp.random.shuffle(self.indices)
+        self._generate_local_seed()
 
     def __iter__(self):
         self.stop()
@@ -257,12 +266,7 @@ class DataLoader:
         # shuffle partition indices to bring disparate
         # parts of the dataset "close" to one another
         if self.shuffle:
-            self._generate_local_seed()
-            if self.seed_fn:
-                new_seed = self.seed_fn()
-                cp.random.seed(new_seed)
-            cp.random.shuffle(self.indices)
-            self._generate_local_seed()
+            self._shuffle_indices()
 
         # build and start new threads for loading and
         # concatenating data
