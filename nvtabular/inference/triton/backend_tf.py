1b70f8db02217b36651129ceadc90cbda8a1b09f
Onur Yilmaz <oyilmaz@nvidia.com>
2021-05-25 12:46:50 -0400

    Update backend_tf

--- nvtabular/inference/triton/backend_tf.py
+++ nvtabular/inference/triton/backend_tf.py
@@ -1,3 +1,29 @@
+# Copyright (c) 2021, NVIDIA CORPORATION. All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#  * Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+#  * Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#  * Neither the name of NVIDIA CORPORATION nor the names of its
+#    contributors may be used to endorse or promote products derived
+#    from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS ``AS IS'' AND ANY
+# EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+# PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+# PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+# OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 import cudf
 import numpy as np
 from cudf.utils.dtypes import is_list_dtype
@@ -5,16 +31,16 @@ from cudf.utils.dtypes import is_list_dtype
 import nvtabular
 
 
+# Empty class that is needed by the numpy array interface
 class numpy_holder(object):
     pass
 
 
 class TritonNVTabularModel:
-    def initialize(self, path_workflow, dtypes):
+    """TritonNVTabularModel for nvtabular C++ backend"""
+
+    def initialize(self, path_workflow, dtypes):        
         self.workflow = nvtabular.Workflow.load(path_workflow)
-        self.input_multihots = {
-            col: dtype for col, dtype in self.workflow.input_dtypes.items() if is_list_dtype(dtype)
-        }
         self.input_dtypes = {
             col: dtype
             for col, dtype in self.workflow.input_dtypes.items()
@@ -31,8 +57,13 @@ class TritonNVTabularModel:
         self.lenghts = list()
 
     def transform(self, input_names, inputs, output_names):
-        input_df = cudf.DataFrame()
+        """Transforms the inputs by running through a NVTabular workflow.transform
+        function.
+        """
 
+        # Create numpy arrays using only the pointers. Then, create
+        # cudf using the numpy arrays.
+        input_df = cudf.DataFrame()
         for i in range(len(inputs)):
             inputs[i]["strides"] = None
             holder_in = numpy_holder()
@@ -40,14 +71,18 @@ class TritonNVTabularModel:
             np_in = np.array(holder_in, copy=False)
             input_df[input_names[i]] = np_in
 
+        # Applies nvtabular transforms
         output_df = nvtabular.workflow._transform_partition(input_df, [self.workflow.column_group])
         output = dict()
         self.lenghts = list()
+
+        # Applies nvtabular transforms
         for name in output_df.columns:
             col = output_df[name]
             if is_list_dtype(col.dtype):
                 # convert list values to match TF dataloader
                 values = col.list.leaves.values_host.astype(self.output_dtypes[name + "__values"])
+                values = values.reshape(len(values), 1)
                 output[name + "__values"] = values
                 self.lenghts.append(len(output[name + "__values"]))
 
