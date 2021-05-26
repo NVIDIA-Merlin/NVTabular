ddba16f333e3c1e22ba523eee7109f977ee0d0e4
Julio Perez <jperez@nvidia.com>
2021-05-26 20:13:27 +0000

    checking tf

--- ci/build_and_test.sh
+++ ci/build_and_test.sh
@@ -36,4 +36,5 @@ echo "Building docs"
 make -C docs html SPHINXOPTS="-W -q"
 
 # test out our codebase
+chmod 777 -R .
 py.test --cov-config tests/unit/.coveragerc --cov-report term-missing --cov-report xml --cov-fail-under 70 --cov=. tests/unit/
