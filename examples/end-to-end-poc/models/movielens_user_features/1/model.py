a4196f663411f5eb9e66d6c4def14e44bd9f8fa4
Karl Higley <kmhigley@gmail.com>
2021-06-24 09:06:36 -0400

    :Add a `TritonPythonModel` to fetch user features from Feast

---
+++ examples/end-to-end-poc/models/movielens_user_features/1/model.py
@@ -0,0 +1,118 @@
+
+import json
+import sys
+import traceback
+
+import numpy as np
+import triton_python_backend_utils as pb_utils
+
+from feast import FeatureStore
+
+class TritonPythonModel:
+    """Your Python model must use the same class name. Every Python model
+    that is created must have "TritonPythonModel" as the class name.
+    """
+
+    def initialize(self, args):
+        """`initialize` is called only once when the model is being loaded.
+        Implementing `initialize` function is optional. This function allows
+        the model to initialize any state associated with this model.
+
+        Parameters
+        ----------
+        args : dict
+          Both keys and values are strings. The dictionary keys and values are:
+          * model_config: A JSON string containing the model configuration
+          * model_instance_kind: A string containing model instance kind
+          * model_instance_device_id: A string containing model instance device ID
+          * model_repository: Model repository path
+          * model_version: Model version
+          * model_name: Model name
+        """
+        self.model_config = json.loads(args["model_config"])
+        repo_path = self.model_config["parameters"]["feast_repo_path"]["string_value"]
+
+        self.store = FeatureStore(repo_path=repo_path)
+
+    def execute(self, requests):
+        """`execute` must be implemented in every Python model. `execute`
+        function receives a list of pb_utils.InferenceRequest as the only
+        argument. This function is called when an inference is requested
+        for this model.
+
+        Parameters
+        ----------
+        requests : list
+          A list of pb_utils.InferenceRequest
+
+        Returns
+        -------
+        list
+          A list of pb_utils.InferenceResponse. The length of this list must
+          be the same as `requests`
+        """
+
+        output_dtype = pb_utils.triton_string_to_numpy("TYPE_INT32")
+
+        responses = []
+
+        # Every Python backend must iterate through list of requests and create
+        # an instance of pb_utils.InferenceResponse class for each of them. You
+        # should avoid storing any of the input Tensors in the class attributes
+        # as they will be overridden in subsequent inference requests. You can
+        # make a copy of the underlying NumPy array and store it if it is
+        # required.
+        for request in requests:
+            # Perform inference on the request and append it to responses list...
+            try:
+              user_ids = pb_utils.get_input_tensor_by_name(request, "user_id").as_numpy()
+
+              entity_rows = [{"user_id": int(user_id)} for user_id in user_ids]
+
+              feature_vector = self.store.get_online_features(
+                  feature_refs=[
+                      'user_features:search_terms',
+                      'user_features:genres',
+                      'user_features:movie_ids'
+                  ],
+                  entity_rows=entity_rows
+              ).to_dict()
+
+              movie_ids = pb_utils.Tensor("movie_ids",
+                            np.array(
+                              feature_vector["user_features__movie_ids"]
+                            ).astype(output_dtype)
+                          )
+
+              genres = pb_utils.Tensor("genres",
+                        np.array(
+                          feature_vector["user_features__genres"]
+                        ).astype(output_dtype)
+                      )
+
+              search_terms = pb_utils.Tensor("search_terms",
+                              np.array(
+                                  feature_vector["user_features__search_terms"]
+                              ).astype(output_dtype)
+                            )
+
+              responses.append(pb_utils.InferenceResponse(
+                output_tensors=[movie_ids, genres, search_terms]
+              ))
+            except Exception as e:
+              responses.append(pb_utils.InferenceResponse(
+                output_tensors=[], error=pb_utils.TritonError(
+                  str(traceback.format_tb(sys.exc_info()[-1])))
+                )
+              )
+
+        # You must return a list of pb_utils.InferenceResponse. Length
+        # of this list must match the length of `requests` list.
+        return responses
+
+    def finalize(self):
+        """`finalize` is called only once when the model is being unloaded.
+        Implementing `finalize` function is optional. This function allows
+        the model to perform any necessary clean ups before exit.
+        """
+        print('Cleaning up...')
