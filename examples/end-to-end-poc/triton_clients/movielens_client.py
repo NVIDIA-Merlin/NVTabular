d7a21163e5e3ee9d7e1d32b3493251bc7493ef14
Karl Higley <kmhigley@gmail.com>
2021-06-29 16:31:39 -0400

    Add Milvus Triton model and update ingest for Milvus 2.0

--- examples/end-to-end-poc/triton_clients/movielens_client.py
+++ examples/end-to-end-poc/triton_clients/movielens_client.py
@@ -8,7 +8,7 @@ model_name = "movielens"
 shape = (1, 1)
 
 with httpclient.InferenceServerClient("0.0.0.0:8000") as client:
-    user_id_data = np.random.randint(1, 1000, shape).astype(np.int32)
+    user_id_data = np.random.randint(1, 100000, shape).astype(np.int32)
     inputs = [
         httpclient.InferInput(
             "user_id", user_id_data.shape, np_to_triton_dtype(user_id_data.dtype)
@@ -18,15 +18,18 @@ with httpclient.InferenceServerClient("0.0.0.0:8000") as client:
     inputs[0].set_data_from_numpy(user_id_data)
 
     outputs = [
-        httpclient.InferRequestedOutput("user_vector"),
+        httpclient.InferRequestedOutput("candidate_ids"),
+        httpclient.InferRequestedOutput("candidate_distances")
     ]
 
     response = client.infer(model_name, inputs, request_id=str(1), outputs=outputs)
 
     result = response.get_response()
+
     print(
-        "user_id: {}, user_vector: {}".format(
+        "user_id:\n{}\n candidate_ids\n{}\n".format(
             user_id_data,
-            response.as_numpy("user_vector"),
+            response.as_numpy("candidate_ids"),
+            # response.as_numpy("candidate_distances"),
         )
     )
