cf3feabac61472c75f98ae81dc6ab204797a183b
Ben Frederickson <github@benfrederickson.com>
2021-06-30 09:46:18 -0700

    Add more tests for triton inference (#913)
    * Add more tests for triton inference
    
    Add the unit tests from the nvtabular_triton_backend repo, as well as testing
    that we can start tritonserver with the output of the TF movielens example.
    
    * fix unittest for when nvtabular c++ backend is unavailable
    
    * Run movielens triton notebook too

--- tests/unit/test_triton_inference.py
+++ tests/unit/test_triton_inference.py
@@ -6,6 +6,7 @@ import time
 from distutils.spawn import find_executable
 
 import cudf
+import numpy as np
 import pytest
 from cudf.tests.utils import assert_eq
 
@@ -16,13 +17,22 @@ triton = pytest.importorskip("nvtabular.inference.triton")
 grpcclient = pytest.importorskip("tritonclient.grpc")
 tritonclient = pytest.importorskip("tritonclient")
 
+TRITON_SERVER_PATH = find_executable("tritonserver")
 
-_TRITON_SERVER_PATH = find_executable("tritonserver")
+
+BACKEND = "python"
+if os.path.exists("/opt/tritonserver/backends/nvtabular/libtriton_nvtabular.so"):
+    BACKEND = "nvtabular"
 
 
 @contextlib.contextmanager
 def run_triton_server(modelpath):
-    cmdline = [_TRITON_SERVER_PATH, "--model-repository", modelpath]
+    cmdline = [
+        TRITON_SERVER_PATH,
+        "--model-repository",
+        modelpath,
+        "--backend-config=tensorflow,version=2",
+    ]
     env = os.environ.copy()
     env["CUDA_VISIBLE_DEVICES"] = "0"
     with subprocess.Popen(cmdline, env=env) as process:
@@ -30,6 +40,10 @@ def run_triton_server(modelpath):
             with grpcclient.InferenceServerClient("localhost:8001") as client:
                 # wait until server is ready
                 for _ in range(60):
+                    if process.poll() is not None:
+                        retcode = process.returncode
+                        raise RuntimeError(f"Tritonserver failed to start (ret={retcode})")
+
                     try:
                         ready = client.is_server_ready()
                     except tritonclient.utils.InferenceServerException:
@@ -47,26 +61,96 @@ def run_triton_server(modelpath):
             process.send_signal(signal.SIGINT)
 
 
-@pytest.mark.skipif(_TRITON_SERVER_PATH is None, reason="Requires tritonserver on the path")
-def test_tritonserver_inference_string(tmpdir):
-    df = cudf.DataFrame({"user": ["aaaa", "bbbb", "cccc", "aaaa", "bbbb", "aaaa"]})
-    features = ["user"] >> ops.Categorify()
-    workflow = nvt.Workflow(features)
-
+def _verify_workflow_on_tritonserver(tmpdir, workflow, df, model_name):
+    """tests that the nvtabular workflow produces the same results when run locally in the
+    process, and when run in tritonserver"""
     # fit the workflow and test on the input
     dataset = nvt.Dataset(df)
     workflow.fit(dataset)
 
     local_df = workflow.transform(dataset).to_ddf().compute(scheduler="synchronous")
-    model_name = "test_inference_string"
-    triton.generate_nvtabular_model(workflow, model_name, tmpdir + "/test_inference_string")
+    triton.generate_nvtabular_model(
+        workflow, model_name, tmpdir + f"/{model_name}", backend=BACKEND
+    )
 
-    inputs = triton.convert_df_to_triton_input(["user"], df)
+    inputs = triton.convert_df_to_triton_input(df.columns, df)
     with run_triton_server(tmpdir) as client:
         response = client.infer(model_name, inputs)
-        user_features = response.as_numpy("user")
-        triton_df = cudf.DataFrame({"user": user_features.reshape(user_features.shape[0])})
-        assert_eq(triton_df, local_df)
+
+        for col in df.columns:
+            features = response.as_numpy(col)
+            triton_df = cudf.DataFrame({col: features.reshape(features.shape[0])})
+            assert_eq(triton_df, local_df[[col]])
+
+
+@pytest.mark.skipif(TRITON_SERVER_PATH is None, reason="Requires tritonserver on the path")
+def test_error_handling(tmpdir):
+    df = cudf.DataFrame({"x": np.arange(10), "y": np.arange(10)})
+
+    def custom_transform(col):
+        if len(col) == 2:
+            raise ValueError("Lets cause some problems")
+        return col
+
+    features = ["x", "y"] >> ops.FillMissing() >> ops.Normalize() >> custom_transform
+    workflow = nvt.Workflow(features)
+    workflow.fit(nvt.Dataset(df))
+
+    model_name = "test_error_handling"
+    triton.generate_nvtabular_model(
+        workflow, model_name, tmpdir + f"/{model_name}", backend=BACKEND
+    )
+
+    with run_triton_server(tmpdir) as client:
+        inputs = triton.convert_df_to_triton_input(["x", "y"], df[:2])
+        with pytest.raises(tritonclient.utils.InferenceServerException) as exception_info:
+            client.infer(model_name, inputs)
+
+        assert "ValueError: Lets cause some problems" in str(exception_info.value)
+
+
+@pytest.mark.skipif(TRITON_SERVER_PATH is None, reason="Requires tritonserver on the path")
+def test_tritonserver_inference_string(tmpdir):
+    df = cudf.DataFrame({"user": ["aaaa", "bbbb", "cccc", "aaaa", "bbbb", "aaaa"]})
+    features = ["user"] >> ops.Categorify()
+    workflow = nvt.Workflow(features)
+    _verify_workflow_on_tritonserver(tmpdir, workflow, df, "test_inference_string")
+
+
+@pytest.mark.skipif(TRITON_SERVER_PATH is None, reason="Requires tritonserver on the path")
+def test_large_strings(tmpdir):
+    strings = ["a" * (2 ** exp) for exp in range(1, 17)]
+    df = cudf.DataFrame({"description": strings})
+    features = ["description"] >> ops.Categorify()
+    workflow = nvt.Workflow(features)
+    _verify_workflow_on_tritonserver(tmpdir, workflow, df, "test_large_string")
+
+
+@pytest.mark.skipif(TRITON_SERVER_PATH is None, reason="Requires tritonserver on the path")
+def test_numeric_dtypes(tmpdir):
+    dtypes = []
+    for width in [8, 16, 32, 64]:
+        dtype = f"int{width}"
+        dtypes.append((dtype, np.iinfo(dtype)))
+        dtype = f"uint{width}"
+        dtypes.append((dtype, np.iinfo(dtype)))
+
+    for width in [32, 64]:
+        dtype = f"float{width}"
+        dtypes.append((dtype, np.finfo(dtype)))
+
+    def check_dtypes(col):
+        assert str(col.dtype) == col.name
+        return col
+
+    # simple transform to make sure we can round-trip the min/max values for each dtype,
+    # through triton, with the 'transform' here just checking that the dtypes are correct
+    df = cudf.DataFrame(
+        {dtype: np.array([limits.max, 0, limits.min], dtype=dtype) for dtype, limits in dtypes}
+    )
+    features = nvt.ColumnGroup(df.columns) >> check_dtypes
+    workflow = nvt.Workflow(features)
+    _verify_workflow_on_tritonserver(tmpdir, workflow, df, "test_numeric_dtypes")
 
 
 def test_generate_triton_multihot(tmpdir):
