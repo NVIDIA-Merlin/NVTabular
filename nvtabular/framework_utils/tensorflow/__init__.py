0e39241944fbddcaa8414d1781813bd5cf8e0c8a
Marc Romeyn <mromeijn@nvidia.com>
2021-07-02 14:49:14 +0200

    Move BlockWithHead to separate file for tf as well

--- nvtabular/framework_utils/tensorflow/__init__.py
+++ nvtabular/framework_utils/tensorflow/__init__.py
@@ -19,7 +19,8 @@ from .feature_column_utils import make_feature_column_workflow
 from .features import (FilterFeatures, ConcatFeatures, StackFeatures, TabularLayer, AsSparseLayer,
                        AsDenseLayer, ParseTokenizedText, AsTabular)
 from .layers.inputs import EmbeddingsLayer, TransformersTextEmbedding, InputFeatures
-from .blocks.base import right_shift_layer, Block, BlockWithHead, TabularBlock, SequentialBlock
+from .blocks.base import right_shift_layer, Block, TabularBlock, SequentialBlock
+from .blocks.with_head import BlockWithHead
 from .blocks.dlrm import DLRMBlock
 from ...loader.tensorflow import KerasSequenceLoader
 from .heads import Head, Task
--- nvtabular/framework_utils/tensorflow/blocks/base.py
+++ nvtabular/framework_utils/tensorflow/blocks/base.py
@@ -5,11 +5,11 @@ import tensorflow as tf
 
 from nvtabular.framework_utils.tensorflow import features
 from nvtabular.framework_utils.tensorflow.heads import Head
-from nvtabular.framework_utils.tensorflow.tfrs import Model
 
 
 class BlockMixin:
     def to_model(self, head: Head, **kwargs):
+        from nvtabular.framework_utils.tensorflow.blocks.with_head import BlockWithHead
         return BlockWithHead(self, head, **kwargs)
 
 
@@ -153,25 +153,6 @@ class SequentialBlock(TabularBlock):
 
 
 BlockType = Union[tf.keras.layers.Layer, Block]
-
-
-class BlockWithHead(Model):
-    def __init__(self, block: BlockType, head: Head, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.block = block
-        self.head = head
-
-    def call(self, inputs, **kwargs):
-        return self.head(self.block(inputs, **kwargs), **kwargs)
-
-    def compute_loss(self, inputs, training: bool = False) -> tf.Tensor:
-        targets = self.head.pop_labels(inputs)
-        logits = self(inputs, training=training)
-
-        return self.head.compute_loss(targets, logits)
-
-    def get_config(self):
-        pass
 
 
 def right_shift_layer(self, other):
