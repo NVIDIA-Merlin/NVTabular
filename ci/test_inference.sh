87bd9f7038696b0e559970bb2155238b10d7b605
Alberto Alvarez <albertoa@nvidia.com>
2021-07-15 19:29:16 -0700

    Removes old scripts

--- ci/test_inference.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cd /nvtabular/
-git pull origin main
-
-tritonserver --model-repository /model/models --backend-config=tensorflow,version=2 --model-control-mode=explicit &
-sleep 1m
-
-pytest tests/integration/test_notebooks.py::test_tf_inference_multihot_examples
--- ci/test_training.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cd /nvtabular/
-git pull origin main
-
-pytest -s tests/integration/test_notebooks.py::test_rossman_example
