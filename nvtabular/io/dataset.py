d277101b685d3c9b654bd7d9c77bc601c02dbbea
rjzamora <rzamora217@gmail.com>
2021-07-19 14:13:08 -0700

    introduce  argument to make algorithm choice more explicit

--- nvtabular/io/dataset.py
+++ nvtabular/io/dataset.py
@@ -608,6 +608,7 @@ class Dataset:
         labels=None,
         suffix=".parquet",
         partition_on=None,
+        method="subgraph",
     ):
         """Writes out to a parquet dataset
 
@@ -636,16 +637,17 @@ class Dataset:
             supported.
         preserve_files : bool
             Whether to preserve the original file-to-partition mapping of
-            the base dataset. This option is only available if the base
-            dataset is known, and if it corresponds to csv or parquet format.
-            If True, the `out_files_per_proc` option will be ignored. Default
-            is False.
+            the base dataset. This option requires `method="subgraph"`, and is
+            only available if the base dataset is known, and if it corresponds
+            to csv or parquet format. If True, the `out_files_per_proc` option
+            will be ignored. Default is False.
         output_files : dict, list or int
-            The total number of desired output files. The default value will
-            be the number of Dask workers, multiplied by `out_files_per_proc`.
-            For further output-file control, this argument may also be used to
-            pass a dictionary mapping the output file names to partition indices,
-            or a list of desired output-file names.
+            The total number of desired output files. This option requires
+            `method="subgraph"`, and the default value will be the number of Dask
+            workers, multiplied by `out_files_per_proc`. For further output-file
+            control, this argument may also be used to pass a dictionary mapping
+            the output file names to partition indices, or a list of desired
+            output-file names.
 
             NOTES:
             (1) If a list of file names is specified, a contiguous range of output
@@ -659,19 +661,17 @@ class Dataset:
             (3) Passing a list or integer to `output_files` will preserve the
                 original ordering of the input data as long as `out_files_per_proc`
                 is set to `1` (or `None`), and `shuffle==None`.
-            (4) For legacy `to_parquet`/`out_files_per_proc` behavior, use
-                `output_files=False`.
-            (5) If a dictionary is specified, excluded partition indices will
+            (4) If a dictionary is specified, excluded partition indices will
                 not be written to disk.
-            (6) To map multiple output files to a range of input partitions,
+            (5) To map multiple output files to a range of input partitions,
                 dictionary-input keys should correspond to a tuple of file names.
         out_files_per_proc : integer
-            Number of output files that each process will use to shuffle each input
-            partition. If `output_files` is not set to `False`, `out_files_per_proc`
-            will not modify the total output-file count, but will instead be
-            interpreted as the number of files to write out within the same task
-            (i.e. process) at run time. This argument should not be specified if
-            a dictionary is passed to `output_files`. Otherwise, the default is 1.
+            Number of output files that each process will use to shuffle an input
+            partition. Deafult is 1. If `method="worker"`, the total number of output
+            files will always be the total number of Dask workers, multiplied by this
+            argument. If `method="subgraph"`, the total number of files is determined
+            by `output_files` (and `out_files_per_proc` must be 1 if a dictionary is
+            specified).
         num_threads : integer
             Number of IO threads to use for writing the output dataset.
             For `0` (default), no dedicated IO threads will be used.
@@ -691,8 +691,38 @@ class Dataset:
             List of continuous columns
         labels : list of str, optional
             List of label columns
+        method : {"subgraph", "worker"}
+            General algorithm to use for the parallel graph execution. In order
+            to minimize memory pressure, `to_parquet` will use a `"subgraph"` by
+            default. This means that we segment the full Dask task graph into a
+            distinct subgraph for each output file (or output-file group). Then,
+            each of these subgraphs is executed, in full, by the same worker (as
+            a single large task). In some cases, it may be more ideal to prioritize
+            concurrency. In that case, a worker-based approach can be used by
+            specifying `method="worker"`.
         """
 
+        # Check that method (algorithm) is valid
+        if method not in ("subgraph", "worker"):
+            raise ValueError(f"{method} not a recognized method for `Dataset.to_parquet`")
+
+        # Deal with method-specific defaults
+        if method == "worker":
+            if output_files or preserve_files:
+                raise ValueError("output_files and preserve_files require `method='subgraph'`")
+            output_files = False
+        elif preserve_files and output_files:
+            raise ValueError("Cannot specify both preserve_files and output_files.")
+        elif not (output_files or preserve_files):
+            # Default "subgraph" behavior - Set output_files to the
+            # total umber of workers, multiplied by out_files_per_proc
+            try:
+                nworkers = len(self.client.cluster.workers)
+            except AttributeError:
+                nworkers = 1
+            output_files = nworkers * (out_files_per_proc or 1)
+
+        # Check shuffle argument
         shuffle = _check_shuffle_arg(shuffle)
 
         if isinstance(output_files, dict) or (not output_files and preserve_files):
@@ -705,20 +735,7 @@ class Dataset:
         # Replace None/False suffix argument with ""
         suffix = suffix or ""
 
-        # Make sure the user is not trying to use both preserve_files
-        # and output_files at the same time
-        if preserve_files and output_files:
-            raise ValueError("Cannot specify both preserve_files and output_files.")
-
-        # Default behavior - Set output_files to the total
-        # number of workers, multiplied by out_files_per_proc
-        if output_files is None and not preserve_files:
-            try:
-                nworkers = len(self.client.cluster.workers)
-            except AttributeError:
-                nworkers = 1
-            output_files = nworkers * (out_files_per_proc or 1)
-
+        # Deal with `method=="subgraph"`.
         # Convert `output_files` argument to a dict mapping
         if output_files:
 
