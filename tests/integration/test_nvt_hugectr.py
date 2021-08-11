5a8e7978b1a4f55a7e8e06039c987800acf2428a
Karl Higley <kmhigley@gmail.com>
2021-08-11 16:01:30 -0400

    Fix a few uses of `WorkflowNode` instead of `ColumnSelector`

--- tests/integration/test_nvt_hugectr.py
+++ tests/integration/test_nvt_hugectr.py
@@ -88,7 +88,7 @@ def test_training():
 
     # Perform ETL with NVTabular
     cat_features = CATEGORICAL_COLUMNS >> nvt.ops.Categorify(cat_cache="device")
-    ratings = nvt.WorkflowNode(["rating"]) >> (lambda col: (col > 3).astype("int8"))
+    ratings = nvt.ColumnSelector(["rating"]) >> (lambda col: (col > 3).astype("int8"))
     output = cat_features + ratings
 
     workflow = nvt.Workflow(output)
