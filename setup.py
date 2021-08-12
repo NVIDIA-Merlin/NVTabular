9a54e6fc46ba460772a997e1b9c84c85e542fd6c
Ben Frederickson <github@benfrederickson.com>
2021-08-12 05:47:39 -0700

    Bump black/isort/flake8 versions (#1029)
    * Bump black/isort/flake8 versions
    
    * Update fill.py

--- setup.py
+++ setup.py
@@ -82,7 +82,7 @@ cmdclass["build_ext"] = build_pybind_and_proto
 
 
 def parse_requirements(filename):
-    """ load requirements from a pip requirements file """
+    """load requirements from a pip requirements file"""
     lineiter = (line.strip() for line in open(filename))
     return [line for line in lineiter if line and not line.startswith("#")]
 
