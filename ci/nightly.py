6eb61ec1fdeb26001cfe2bf3b6f668b2e98e9056
Alberto Alvarez <albertoa@nvidia.com>
2021-08-18 23:40:09 -0700

    Initial nightly script

--- ci/nightly.py
+++ ci/nightly.py
@@ -1,10 +1,17 @@
 from tabulate import tabulate
 import argparse
+import smtplib
 
 # Get parameters
 parser = argparse.ArgumentParser(description='Pushes nightly nvbug release')
 parser.add_argument('--cont', type=str, help='Merlin container')
 parser.add_argument('--qa', type=str, help='Container tests results')
+parser.add_argument('--server', type=str, help='Server to send email')
+parser.add_argument('--port', type=str, help='Port to send email')
+parser.add_argument('--username', type=str, help='Username to send email')
+parser.add_argument('--password', type=str, help='Password to send email')
+parser.add_argument('--sender', type=str, help='Email sender')
+parser.add_argument('--receiver', type=str, help='Email receiver')
 args = parser.parse_args()
 
 # Set date
@@ -19,4 +26,11 @@ mydata = [["1", "NGC catalog URL of the container", args.cont],
           ["9", "AWS Federation Google form completed", "No"]]
   
 # create table
-print(tabulate(mydata, tablefmt="grid"))
+body = tabulate(mydata, tablefmt="grid")
+
+# Send email
+server = smtplib.SMTP(args.server, args.port)
+server.login(args.username, args.password)
+
+header = "/n"
+server.sendmail(args.sender, args.receiver, header+body)
