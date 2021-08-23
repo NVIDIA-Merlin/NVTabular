3fa0ce10b508b88add605ee18cb6cd1c0c142c3b
rjzamora <rzamora@nvidia.com>
2021-08-23 12:02:09 -0500

    save experimental fixes to pandas nullable-type problem

--- nvtabular/ops/categorify.py
+++ nvtabular/ops/categorify.py
@@ -34,7 +34,7 @@ from fsspec.core import get_fs_token_paths
 from pyarrow import parquet as pq
 
 from nvtabular import dispatch
-from nvtabular.dispatch import DataFrameType, annotate
+from nvtabular.dispatch import DataFrameType, annotate, _null_series
 from nvtabular.worker import fetch_table_data, get_worker_cache
 
 from .operator import ColumnNames, Operator
@@ -399,7 +399,8 @@ class Categorify(StatOperator):
 
         # Encode each column-group separately
         for name in cat_names:
-            try:
+            #try:
+            if True:
                 # Use the column-group `list` directly (not the string name)
                 use_name = multi_col_group.get(name, name)
 
@@ -433,8 +434,8 @@ class Categorify(StatOperator):
                     max_size=self.max_size,
                     dtype=self.dtype,
                 )
-            except Exception as e:
-                raise RuntimeError(f"Failed to categorical encode column {name}") from e
+            # except Exception as e:
+            #     raise RuntimeError(f"Failed to categorical encode column {name}") from e
 
         return new_df
 
@@ -858,7 +859,7 @@ def _write_uniques(dfs, base_path, col_group, options):
 
                 nulls_missing = True
                 new_cols[col] = _concat(
-                    [df._constructor_sliced([None], dtype=df[col].dtype), df[col]],
+                    [_null_series(df, df[col].dtype), df[col]],
                     ignore_index=True,
                 )
             else:
@@ -1045,7 +1046,7 @@ def _encode(
         value = type(df)()
         for c in selection_r:
             typ = df[selection_l[0]].dtype if len(selection_l) == 1 else df[c].dtype
-            value[c] = df._constructor_sliced([None], dtype=typ)
+            value[c] = _null_series(df, typ)
         value.index.name = "labels"
         value.reset_index(drop=False, inplace=True)
 
@@ -1055,8 +1056,8 @@ def _encode(
             codes["order"] = dispatch._arange(len(codes), like_df=df)
         else:
             codes = type(df)({"order": dispatch._arange(len(df), like_df=df)}, index=df.index)
-            for c in selection_l:
-                codes[c] = df[c].copy()
+            for cl, cr in zip(selection_l, selection_r):
+                codes[cl] = df[cl].copy().astype(value[cr].dtype)
         if buckets and storage_name in buckets:
             na_sentinel = _hash_bucket(df, buckets, selection_l, encode_type=encode_type)
         # apply frequency hashing
