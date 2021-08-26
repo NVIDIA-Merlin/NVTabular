12094723e5771017c02f182515c50efee1e4884b
Karl Higley <kmhigley@gmail.com>
2021-08-26 14:54:57 -0400

    Fix the combined parent/root schema passed to nodes in schema fitting

--- nvtabular/workflow/workflow.py
+++ nvtabular/workflow/workflow.py
@@ -145,6 +145,7 @@ class Workflow:
                         [parent.output_schema for parent in node.parents if parent.output_schema],
                         Schema(),
                     )
+                    combined_schema += input_schema
                     node.compute_schemas(combined_schema)
 
                 processed_nodes.append(node)
