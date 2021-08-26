4ebb6bc11da12f2a0034dccc8f59701dd2240061
Karl Higley <karlb@nvidia.com>
2021-08-26 17:00:10 -0400

    Refactor out the `WorkflowNode.kind` property and fix `WorkflowNode.__sub__` (#1069)
    * Insert a `ConcatColumns` operator on `+` nodes in the `Workflow` graph
    
    * Insert a `SubsetColumns` operator on `-` nodes in the `Workflow` graph
    
    * Insert `SubsetColumns` operator on `[]` nodes in the `Workflow` graph
    
    * Create and apply an `Identity` operator
    
    This allows all `WorkflowNodes` (even temporary/virtual nodes used in graph construction) to have an operator.
    
    * Add `Concat/SubsetColumns.label` property to operators replacing `kind`
    
    Add `Concat/SubsetColumns.label` property to operators replacing `kind`
    
    * Split `Workflow.fit_schema` tests into a separate file
    
    * Fix the combined parent/root schema passed to nodes in schema fitting
    
    * Fix `WorkflowNode.__sub__`
    
    * Make `ColumnSelector.names` return only unique column names
    
    This avoids a CPU-only bug related to the dataframes that Pandas creates when supplied with duplicate column names in `df[col_selector.names]`, which include the same columns more than once.

---
+++ tests/unit/workflow/test_workflow_ops.py
@@ -0,0 +1,37 @@
+#
+# Copyright (c) 2021, NVIDIA CORPORATION.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+from nvtabular import ops
+from nvtabular.ops import internal
+
+
+def test_column_concat_op():
+    node = "col1" >> ops.Operator()
+    col_name = "col2"
+
+    concat_node = node + col_name
+    assert isinstance(concat_node.op, internal.ConcatColumns)
+
+
+def test_column_subset_op():
+    node = ["col1", "col2"] >> ops.Operator()
+    col_name = "col1"
+
+    subtract_node = node - col_name
+    assert isinstance(subtract_node.op, internal.SubsetColumns)
+
+    bracket_node = node["col1"]
+    assert isinstance(bracket_node.op, internal.SubsetColumns)
