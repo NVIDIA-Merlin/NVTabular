c39e36213b5d5061b568b59ca997a2537089cfdf
Karl Higley <kmhigley@gmail.com>
2021-08-31 14:51:19 -0400

    Use node selector for `input_columns` (if it exists)

--- nvtabular/workflow/node.py
+++ nvtabular/workflow/node.py
@@ -250,7 +250,10 @@ class WorkflowNode:
 
     @property
     def input_columns(self):
-        return ColumnSelector(self.input_schema.column_names)
+        if self.selector:
+            return self.selector
+        else:
+            return ColumnSelector(self.input_schema.column_names)
 
     @property
     def output_columns(self):
