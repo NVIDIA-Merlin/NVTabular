cdc9fb23a0dd8786febced29330bb48fb8f3282e
Julio Perez <37191411+jperez999@users.noreply.github.com>
2021-09-16 13:48:40 -0400

    Test Import shuffle (#1118)
    * fixing placement of teesting to support current structure of imports
    
    * changes for formatting
    
    * remove unused import

--- tests/unit/loader/test_dataloader_backend.py
+++ tests/unit/loader/test_dataloader_backend.py
@@ -13,18 +13,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-
-import os
-
 import cupy
 import pytest
 
-import nvtabular as nvt
-import nvtabular.ops as ops
 from nvtabular.io.dataset import Dataset
 from nvtabular.loader.backend import DataLoader
-from nvtabular.loader.tensorflow import KerasSequenceLoader
-from nvtabular.loader.torch import TorchAsyncItr
 
 
 @pytest.mark.parametrize("engine", ["parquet"])
@@ -110,57 +103,3 @@ def test_dataloader_seeding(datasets, engine, batch_size):
     # Test that after the shuffle each worker generates different random numbers
     # (i.e. the random seeds are different on each worker after the shuffle)
     assert dl0_next_rand != dl1_next_rand
-
-
-@pytest.mark.parametrize("batch_size", [1000])
-@pytest.mark.parametrize("engine", ["parquet"])
-@pytest.mark.parametrize("device", [None, 0])
-@pytest.mark.parametrize("framework", ["tf", "torch"])
-def test_dataloader_schema(tmpdir, df, dataset, batch_size, engine, device, framework):
-    cat_names = ["name-cat", "name-string"]
-    cont_names = ["x", "y", "id"]
-    label_name = ["label"]
-
-    conts = cont_names >> ops.FillMedian() >> ops.Normalize()
-    cats = cat_names >> ops.Categorify()
-
-    processor = nvt.Workflow(conts + cats + label_name)
-
-    output_train = os.path.join(tmpdir, "train/")
-    os.mkdir(output_train)
-
-    processor.fit_transform(dataset).to_parquet(
-        shuffle=nvt.io.Shuffle.PER_PARTITION,
-        output_path=output_train,
-        out_files_per_proc=2,
-    )
-
-    tar_paths = [
-        os.path.join(output_train, x) for x in os.listdir(output_train) if x.endswith("parquet")
-    ]
-
-    nvt_data = nvt.Dataset(tar_paths, engine="parquet")
-
-    if framework == "torch":
-        data_loader = TorchAsyncItr(
-            nvt_data,
-            batch_size=batch_size,
-            shuffle=False,
-            labels=label_name,
-        )
-    elif framework == "tf":
-        data_loader = KerasSequenceLoader(
-            nvt_data,
-            batch_size=batch_size,
-            shuffle=False,
-            label_names=label_name,
-        )
-    else:
-        assert False, "Unknown dataloader framework"
-
-    batch = next(iter(data_loader))
-    assert all(name in batch[0] for name in cat_names)
-    assert all(name in batch[0] for name in cont_names)
-
-    num_label_cols = batch[1].shape[1] if len(batch[1].shape) > 1 else 1
-    assert num_label_cols == len(label_name)
