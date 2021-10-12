d99aa25422b58bae0b16816a627b44a649c1d757
Julio Perez <jperez@nvidia.com>
2021-10-12 00:33:30 +0000

    writer base remove extra line

--- nvtabular/columns/schema_io/schema_writer_base.py
+++ nvtabular/columns/schema_io/schema_writer_base.py
@@ -1,5 +1,4 @@
-class SchemaWriter():
-
+class SchemaWriter:
     @classmethod
     def _read_scheam(cls, schema_path):
         raise NotImplementedError("Must have logic to read schema from file")
@@ -8,7 +7,6 @@ class SchemaWriter():
     def write_schema(cls, schema, schema_path):
         raise NotImplementedError("Must have logic to write schema to file")
 
-    @staticmethod
+    @classmethod
     def load_schema(cls, schema_path):
         raise NotImplementedError("logic to create schema object from file")
-
