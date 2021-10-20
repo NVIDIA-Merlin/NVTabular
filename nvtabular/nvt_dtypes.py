73c0e9090e236fb7e1e8f266c6b0aea900f2fed0
Julio Perez <jperez@nvidia.com>
2021-10-20 14:23:57 -0400

    basic testing up for all branches

--- nvtabular/nvt_dtypes.py
+++ nvtabular/nvt_dtypes.py
@@ -18,65 +18,94 @@ from enum import Enum
 
 # import cudf
 import numpy as np
+from cudf._typings import Dtype
 
 # import pandas as pd
+from cudf.core.dtypes import dtype
+from pandas.core.dtypes.dtypes import CategoricalDtype, PandasExtensionDtype
+from pandas.core.dtypes.inference import is_nested_list_like
 
 
 class PandasDtypes:
     @classmethod
-    def _to(cls, dtype):
+    def _to(cls, nvt_dtype):
         # return pandas dtype, input must be nvt.dtype
-        pass
+        if not isinstance(nvt_dtype, NVTDtype):
+            raise ValueError(
+                f"The supplied dtype({type(nvt_dtype)}) does not extend the NVTDtype class"
+            )
+        return NumpyDtypes._to(nvt_dtype)
 
     @classmethod
-    def _from(cls, dtype):
-        # return nvt dtype, input must be pandas dtype
-        pass
+    def _from(cls, in_dtype):
+        # return nvt dtype, input must be pandas dtype or numpy base type
+        if isinstance(in_dtype, PandasExtensionDtype):
+            # pandas extended routine
+            name, signed = cls.dtypes_to_nvt[in_dtype.kind]
+            # account for bytes to bits
+            size = in_dtype.itemsize * 8
+            is_list = bool(is_nested_list_like(in_dtype))
+            return NVTDtype(name, size, signed, is_list)
+        return NumpyDtypes._from(in_dtype)
 
-    dtypes_dict = {}
+    dtypes_to_nvt = {CategoricalDtype: ("object", False)}
+
+    nvt_to_dtypes = {"object": CategoricalDtype}
 
 
 class CudfDtypes:
     @classmethod
-    def _to(cls, dtype):
-        # return pandas dtype, input must be nvt.dtype
-        pass
+    def _to(cls, nvt_dtype):
+        # return cudf dtype, input must be nvt.dtype
+        if not isinstance(nvt_dtype, NVTDtype):
+            raise ValueError(
+                f"The supplied dtype({type(nvt_dtype)}) does not extend the NVTDtype class"
+            )
+        return dtype(NumpyDtypes._to(nvt_dtype))
 
     @classmethod
-    def _from(cls, dtype):
+    def _from(cls, in_dtype):
         # return nvt dtype, input must be pandas dtype
-        pass
-
-    dtypes_dict = {}
+        if isinstance(in_dtype, Dtype):
+            # pandas extended routine
+            name, signed = cls.dtypes_to_nvt[in_dtype.kind]
+            # account for bytes to bits
+            size = in_dtype.itemsize * 8
+            is_list = bool(is_nested_list_like(in_dtype))
+            return NVTDtype(name, size, signed, is_list)
+        return NumpyDtypes._from(in_dtype)
 
 
 class NumpyDtypes:
     @classmethod
-    def _to(cls, dtype):
+    def _to(cls, nvt_dtype):
         # return pandas dtype, input must be nvt.dtype
-        if not isinstance(dtype, NVTDtype):
+        if not isinstance(nvt_dtype, NVTDtype):
             raise ValueError(
-                f"The supplied dtype({type(dtype)}) does not extend the NVTDtype class"
+                f"The supplied dtype({type(nvt_dtype)}) does not extend the NVTDtype class"
             )
-        signed = "signed" if dtype.signed else "unsigned"
-        return cls.nvt_to_dtypes[dtype.name][signed][dtype.size]
+        signed = "signed" if nvt_dtype.signed else "unsigned"
+        return cls.nvt_to_dtypes[nvt_dtype.name][signed][nvt_dtype.size]
 
     @classmethod
-    def _from(cls, dtype):
+    def _from(cls, in_dtype):
         # return nvt dtype, input must be pandas dtype
-        if "numpy" not in str(dtype):
+        if "numpy" not in str(in_dtype):
             try:
-                dtype = np.dtype(dtype)
+                in_dtype = np.dtype(in_dtype)
             except TypeError as err:
                 raise ValueError(f"Cannot convert non supported numpy dtype: {dtype}") from err
-        if not isinstance(dtype, np.dtype):
-            dtype = np.dtype(dtype)
-        if dtype.kind not in cls.dtypes_to_nvt:
-            raise ValueError(f"NVTabular does not currently support numpy dtype kind: {dtype.kind}")
-        name, signed = cls.dtypes_to_nvt[dtype.kind]
+        if not isinstance(in_dtype, np.dtype):
+            in_dtype = np.dtype(in_dtype)
+        # TODO: should be checked against enum strings available
+        if in_dtype.kind not in cls.dtypes_to_nvt:
+            raise ValueError(
+                f"NVTabular does not currently support numpy dtype kind: {in_dtype.kind}"
+            )
+        name, signed = cls.dtypes_to_nvt[in_dtype.kind]
         # account for bytes to bits
-        size = dtype.itemsize * 8
-        is_list = bool(dtype.subdtype)
+        size = in_dtype.itemsize * 8
+        is_list = bool(in_dtype.subdtype)
         return NVTDtype(name, size, signed, is_list)
 
     # should this use None (aka unsupported) as a third option
@@ -120,8 +149,8 @@ class NumpyDtypes:
 
 
 class NVT_Dtypes(Enum):
-    INT = ("int",)
-    FLOAT = ("float",)
+    INT = "int"
+    FLOAT = "float"
 
 
 @dataclass(frozen=True)
@@ -144,16 +173,16 @@ class NVTDtype:
         )
 
     @classmethod
-    def _from(cls, dtype):
-        dtype_type = str(dtype)
+    def _from(cls, in_dtype):
+        dtype_type = str(in_dtype)
         for dtype_key in list(cls.dtypes_dict.keys()):
             if dtype_key in dtype_type:
-                return cls.dtypes_dict[dtype_key]._from(dtype)
+                return cls.dtypes_dict[dtype_key]._from(in_dtype)
         try:
-            dtype = np.dtype(dtype)
-            return cls.dtypes_dict["numpy"]._from(dtype)
+            in_dtype = np.dtype(in_dtype)
+            return cls.dtypes_dict["numpy"]._from(in_dtype)
         except TypeError as err:
             raise ValueError(
-                f"""did not find appropriate dtype base for {dtype},
+                f"""did not find appropriate dtype base for {in_dtype},
                 currently support: {cls.dtypes_dict.keys()}"""
             ) from err
---
+++ tests/unit/dtypes/test_cudf_dtypes.py
@@ -0,0 +1,48 @@
+#
+# Copyright (c) 2021, NVIDIA CORPORATION.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+import cudf
+import pytest
+
+from nvtabular.nvt_dtypes import NVTDtype
+
+
+@pytest.mark.parametrize(
+    "dtype_combo",
+    [
+        (
+            cudf.DataFrame(
+                {
+                    "int": [1, 2, 3, 4],
+                    "float": [1.0, 2.0, 3.0, 4.0],
+                }
+            ),
+            ("int", "float"),  # dtype names nvt specific
+            True,  # signed
+        ),
+    ],
+)
+def test_dtypes(dtype_combo):
+    df_pandas, d_names, d_signed = dtype_combo
+    pd_dtypes = df_pandas.dtypes
+    for dname, dtype in zip(d_names, pd_dtypes):
+        nvt_dtype = NVTDtype._from(dtype)
+        assert nvt_dtype.name == dname
+        assert nvt_dtype.size == dtype.itemsize * 8  # translate to btis
+        assert nvt_dtype.signed == d_signed
+        assert not nvt_dtype.is_list
+        # then convert back for testing
+        np_dtype = nvt_dtype._to("cudf")
+        assert np_dtype == dtype
