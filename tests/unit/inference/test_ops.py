449c8a45e132b26988fc9ddfac294fe24a54ae05
Karl Higley <kmhigley@gmail.com>
2021-11-29 11:03:55 -0500

    Test the inference ops

---
+++ tests/unit/inference/test_ops.py
@@ -0,0 +1,65 @@
+import os
+import pathlib
+
+import pytest
+
+# this needs to be before any modules that import protobuf
+os.environ["PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION"] = "python"
+
+from google.protobuf import text_format  # noqa
+
+import nvtabular as nvt  # noqa
+import nvtabular.inference.triton.model_config_pb2 as model_config  # noqa
+import nvtabular.ops as wf_ops  # noqa
+from nvtabular.graph.schema import Schema  # noqa
+from nvtabular.inference.ensemble import Ensemble  # noqa
+from nvtabular.inference.graph.ops.workflow import WorkflowOp  # noqa
+
+
+@pytest.mark.parametrize("engine", ["parquet"])
+def test_workflow_op_validates_schemas(dataset, engine):
+    input_columns = ["x", "y", "id"]
+    request_schema = Schema(input_columns)
+
+    # NVT
+    workflow_ops = input_columns >> wf_ops.Rename(postfix="_nvt")
+    workflow = nvt.Workflow(workflow_ops)
+    workflow.fit(dataset)
+
+    # Triton
+    triton_ops = ["a", "b", "c"] >> WorkflowOp(workflow)
+
+    with pytest.raises(ValueError):
+        Ensemble(triton_ops, request_schema)
+
+
+@pytest.mark.parametrize("engine", ["parquet"])
+def test_workflow_op_exports_own_config(tmpdir, dataset, engine):
+    input_columns = ["x", "y", "id"]
+
+    # NVT
+    workflow_ops = input_columns >> wf_ops.Rename(postfix="_nvt")
+    workflow = nvt.Workflow(workflow_ops)
+    workflow.fit(dataset)
+
+    # Triton
+    triton_op = WorkflowOp(workflow)
+    triton_op.export(tmpdir)
+
+    # Export creates directory
+    export_path = pathlib.Path(tmpdir) / triton_op.export_name
+    assert export_path.exists()
+
+    # Export creates the config file
+    config_path = export_path / "config.pbtxt"
+    assert config_path.exists()
+
+    # Read the config file back in from proto
+    with open(config_path, "rb") as f:
+        config = model_config.ModelConfig()
+        raw_config = f.read()
+        parsed = text_format.Parse(raw_config, config)
+
+        # The config file contents are correct
+        assert parsed.name == "model"
+        assert parsed.backend == "nvtabular"
