9bce2a1683fcf773cb21399d86c98494163d1b93
root <ronayak@hotmail.com>
2021-12-02 14:34:40 +0000

    isort fix

--- nvtabular/inference/triton/ensemble.py
+++ nvtabular/inference/triton/ensemble.py
@@ -28,8 +28,8 @@ from google.protobuf import text_format  # noqa
 
 import nvtabular.inference.triton.model_config_pb2 as model_config  # noqa
 from nvtabular.dispatch import _is_list_dtype, _is_string_dtype  # noqa
-from nvtabular.graph.schema import Schema  # noqa
 from nvtabular.graph.node import iter_nodes  # noqa
+from nvtabular.graph.schema import Schema  # noqa
 
 
 def export_tensorflow_ensemble(
