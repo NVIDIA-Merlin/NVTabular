ffa5eb35ee63d4027c8e8ef0183b133c04bdc55f
Karl Higley <kmhigley@gmail.com>
2021-12-07 14:11:47 -0500

    Add -y to pip uninstall

--- ci/build_and_test.sh
+++ ci/build_and_test.sh
@@ -8,7 +8,7 @@ cd $nvt_directory
 
 echo "Installing NVTabular"
 python -m pip install --user --upgrade pip setuptools wheel pybind11 numpy==1.20.3
-python -m pip uninstall nvtabular
+python -m pip uninstall nvtabular -y
 python setup.py develop --user
 
 # following checks requirement requirements-dev.txt to be installed
