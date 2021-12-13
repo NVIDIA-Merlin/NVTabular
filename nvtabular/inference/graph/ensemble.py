e48f1c7ee363fea99313d1edd3c42fc76b207a62
Julio Perez <37191411+jperez999@users.noreply.github.com>
2021-12-13 15:01:11 -0500

    Apply NVT graph API/DSL to building Triton ensembles (#1292)
    * Extract abstract `Node` class from `WorkflowNode`
    
    * Rename `ops.internal` sub-package to `ops.graph`
    
    * Extract `BaseOperator` class from (Workflow) `Operator`
    
    * Move `ColumnSelector` and `Schema` classes into `graph` package
    
    * fix BaseOperator in Node
    
    * Fix `graph.` imports in Node
    
    * Change parent class of graph operators to `BaseOperator`
    
    * Move graph ops into `graph` package
    
    * Remove `WorkflowNode` dependency from `ColumnSelector`
    
    * Fix LambdaOp tests, assorted imports, ColumnSelector rshift
    
    * Move tags into the `graph` package
    
    * Fix additional LambdaOp tests
    
    * Fix doc files paths for `graph` package
    
    * More docs fixes
    
    * Extract abstract `Graph` class from `Workflow`
    
    * Make graphs track their input schemas
    
    * Fix `Schema.select_by_name()` to return partial matches without erroring
    
    * Create inference node and op base classes
    
    * Create a `Workflow` op for inference
    
    * Add an init file to the inference graph package
    
    * Create an `OpRunner` class to execute ops from a config at serving time
    
    * Test the inference ops
    
    * Test graph schema propagation
    
    * Test the op runner
    
    * workflow changes to handle fit correctly
    
    * working multi workflow op in oprunner for triton
    
    * created test for triton execution, trying out tensordf
    
    * revert formatting on test
    
    * set copyright on test file and added testing and functionality for tensordf
    
    * working tensorflow operator inference
    
    * working workflow + tensorflow inference graph
    
    * working ensemble for happy path workflow + model only
    
    * fully working unit tests with main merge
    
    * working triton ensemble graph creation and e2e test
    
    * fixes to tensorflow ensemble creation and testing
    
    * remove unused imports
    
    * reset imports for correct importing
    
    * str cast tmpdir in tests
    
    * fixes for cpu version of execution
    
    * fix for unit tests to skip if tritonserver not found
    
    * clean up from PR comments
    
    * Update nvtabular/inference/graph/ops/__init__.py
    
    Co-authored-by: Karl Higley <kmhigley@gmail.com>
    
    * making formatting changes
    
    * fixes from review
    
    * default inference operator names all lowercase
    
    Co-authored-by: Karl Higley <kmhigley@gmail.com>

---
+++ nvtabular/inference/graph/ensemble.py
@@ -0,0 +1,96 @@
+#
+# Copyright (c) 2021, NVIDIA CORPORATION.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+import os
+
+# this needs to be before any modules that import protobuf
+os.environ["PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION"] = "python"
+
+from google.protobuf import text_format  # noqa
+
+import nvtabular.inference.triton.model_config_pb2 as model_config  # noqa
+from nvtabular.graph.graph import Graph  # noqa
+from nvtabular.graph.ops.selection import SelectionOp  # noqa
+from nvtabular.inference.graph.ops.tensorflow import TensorflowOp  # noqa
+from nvtabular.inference.graph.ops.workflow import WorkflowOp  # noqa
+
+
+class Ensemble:
+    def __init__(self, ops, schema, name="ensemble_model", label_columns=None):
+        self.graph = Graph(ops)
+        self.graph.fit_schema(schema)
+        self.name = name
+        self.label_columns = label_columns or []
+
+    def export(self, export_path, version=1):
+        parents = self.graph.output_node.parents_with_dependencies
+        assert len(parents) == 1
+
+        workflow_node = parents[0]
+        assert len(workflow_node.parents_with_dependencies) == 1
+        assert isinstance(workflow_node.op, WorkflowOp)
+
+        selection_node = workflow_node.parents_with_dependencies[0]
+        assert len(selection_node.parents_with_dependencies) == 0
+        assert isinstance(selection_node.op, SelectionOp)
+
+        model_node = self.graph.output_node
+        assert isinstance(model_node.op, TensorflowOp)
+
+        nodes_list = [model_node, workflow_node]
+        config = None
+        configs = []
+        for node in nodes_list:
+            config = node.op.export(export_path, config, version=version)
+            configs.append(config)
+
+        # generate the triton ensemble
+        ensemble_path = os.path.join(export_path, self.name)
+        os.makedirs(ensemble_path, exist_ok=True)
+        os.makedirs(os.path.join(ensemble_path, str(version)), exist_ok=True)
+        configs.reverse()
+        return self._generate_ensemble_config(self.name, ensemble_path, configs)
+
+    def _generate_ensemble_config(self, name, output_path, configs, name_ext=""):
+        # TODO: max batchsize only relevant for workflow nodes
+        ensemble_config = model_config.ModelConfig(
+            name=name + name_ext, platform="ensemble", max_batch_size=configs[0].max_batch_size
+        )
+        ensemble_config.input.extend(configs[0].input)
+        ensemble_config.output.extend(configs[-1].output)
+
+        # workflow, model
+        prev_step = None
+        for idx, config in enumerate(configs):
+            config_step = model_config.ModelEnsembling.Step(
+                model_name=config.name, model_version=-1
+            )
+            for input_col in config.input:
+                prev_step_ouputs = dict(prev_step.output_map) if prev_step else {}
+                prev_step_input_col = (
+                    prev_step_ouputs[input_col.name]
+                    if prev_step_ouputs and input_col.name in prev_step_ouputs
+                    else input_col.name
+                )
+                config_step.input_map[input_col.name] = prev_step_input_col
+            for output_col in config.output:
+                out_suffix = f"_{idx + 1}" if idx < len(configs) - 1 else ""
+                config_step.output_map[output_col.name] = output_col.name + out_suffix
+            ensemble_config.ensemble_scheduling.step.append(config_step)
+            prev_step = config_step
+
+        with open(os.path.join(output_path, "config.pbtxt"), "w") as o:
+            text_format.PrintMessage(ensemble_config, o)
+        return ensemble_config
