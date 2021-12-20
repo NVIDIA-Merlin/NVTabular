000aac69737f025b8d9d980862c2d1950e1e9a89
Julio Perez <jperez@nvidia.com>
2021-12-20 12:53:55 -0500

    locking down setuptools for bug in v60.0

--- ci/build_and_test.sh
+++ ci/build_and_test.sh
@@ -7,7 +7,7 @@ nvt_directory="$(dirname -- $ci_directory)"
 cd $nvt_directory
 
 echo "Installing NVTabular"
-python -m pip install --user --upgrade pip setuptools wheel pybind11 numpy==1.20.3
+python -m pip install --user --upgrade pip setuptools wheel pybind11 numpy==1.20.3 setuptools==59.4.0
 python -m pip uninstall nvtabular -y
 python setup.py develop --user
 
