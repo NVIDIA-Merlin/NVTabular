7e64ef15cf9bbc61b67ee69d0ad6fc1d711e0eb1
Julio Perez <37191411+jperez999@users.noreply.github.com>
2022-03-17 15:18:37 -0400

    Reactivate hugectr Criteo integration test (#1457)
    * fixed lambdaop call
    
    * prop change of dtype kwarg in lambda to torch example
    
    * added index migration support for faiss
    
    * added fixes for criteo hugectr support
    
    * fixes for integration
    
    * add updates to integration ci script
    
    * activate hugectr benchmark
    
    * deactivate inference test and activate correct testing
    
    Co-authored-by: Karl Higley <kmhigley@gmail.com>

--- tests/integration/common/parsers/criteo_parsers.py
+++ tests/integration/common/parsers/criteo_parsers.py
@@ -17,7 +17,6 @@ import re
 
 from tests.integration.common.parsers.benchmark_parsers import (
     BenchFastAI,
-    Benchmark,
     StandardBenchmark,
     create_bench_result,
 )
@@ -64,21 +63,29 @@ class CriteoBenchFastAI(BenchFastAI):
         return [t_loss, v_loss]
 
 
-class CriteoBenchHugeCTR(Benchmark):
+class CriteoBenchHugeCTR(StandardBenchmark):
+    def __init__(self, name="CriteoHugeCTR"):
+        self.name = name
+
     def get_epochs(self, output):
         epochs = []
         for line in output:
             if "AUC" in line:
-                epochs.append(self.get_epoch(line))
-        return epochs[-1:]
+                epochs.append(line)
+            if "run_time:" in line:
+                run_time = self.get_runtime(line)
+        if run_time and epochs:
+            return self.get_epoch(epochs[-1], run_time)
+        return []
 
-    def get_epoch(self, line):
-        split_line = line.split(",")
-        iteration = int(split_line[0].split(":")[-1])
-        auc = float(split_line[-1][:-2])
-        bres_auc = create_bench_result(
-            f"{self.name}_auc", [("iteration", iteration)], auc, "percent"
-        )
+    def get_runtime(self, line):
+        split_line = line.split(":")
+        return float(split_line[1])
+
+    def get_epoch(self, line, runtime):
+        split_line = line.split("AUC:")
+        auc = float(split_line[-1])
+        bres_auc = create_bench_result(f"{self.name}_auc", [("time", runtime)], auc, "percent")
         return [bres_auc]
 
 
