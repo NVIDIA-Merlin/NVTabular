2ff2c1658548337151a12525e733725219802e27
Julio Perez <jperez@nvidia.com>
2022-05-24 23:05:38 -0400

    add string type to proto lookups

--- nvtabular/inference/triton/model_config_util.py
+++ nvtabular/inference/triton/model_config_util.py
@@ -8,21 +8,17 @@ if file_descriptor:
     ModelConfig = msg_factory.CreatePrototype(file_descriptor.message_types_by_name["ModelConfig"])
     ModelInput = msg_factory.CreatePrototype(file_descriptor.message_types_by_name["ModelInput"])
     ModelOutput = msg_factory.CreatePrototype(file_descriptor.message_types_by_name["ModelOutput"])
-    TYPE_FP64 = file_descriptor.enum_types_by_name["DataType"].values_by_name["TYPE_FP64"].number
-    TYPE_FP32 = file_descriptor.enum_types_by_name["DataType"].values_by_name["TYPE_FP32"].number
-    TYPE_FP16 = file_descriptor.enum_types_by_name["DataType"].values_by_name["TYPE_FP16"].number
-    TYPE_INT64 = file_descriptor.enum_types_by_name["DataType"].values_by_name["TYPE_INT64"].number
-    TYPE_INT32 = file_descriptor.enum_types_by_name["DataType"].values_by_name["TYPE_INT32"].number
-    TYPE_INT16 = file_descriptor.enum_types_by_name["DataType"].values_by_name["TYPE_INT16"].number
-    TYPE_INT8 = file_descriptor.enum_types_by_name["DataType"].values_by_name["TYPE_INT8"].number
-    TYPE_UINT64 = (
-        file_descriptor.enum_types_by_name["DataType"].values_by_name["TYPE_UINT64"].number
-    )
-    TYPE_UINT32 = (
-        file_descriptor.enum_types_by_name["DataType"].values_by_name["TYPE_UINT32"].number
-    )
-    TYPE_UINT16 = (
-        file_descriptor.enum_types_by_name["DataType"].values_by_name["TYPE_UINT16"].number
-    )
-    TYPE_UINT8 = file_descriptor.enum_types_by_name["DataType"].values_by_name["TYPE_UINT8"].number
-    TYPE_BOOL = file_descriptor.enum_types_by_name["DataType"].values_by_name["TYPE_BOOL"].number
+    enum_descriptor = file_descriptor.enum_types_by_name["DataType"]
+    TYPE_FP64 = enum_descriptor.values_by_name["TYPE_FP64"].number
+    TYPE_FP32 = enum_descriptor.values_by_name["TYPE_FP32"].number
+    TYPE_FP16 = enum_descriptor.values_by_name["TYPE_FP16"].number
+    TYPE_INT64 = enum_descriptor.values_by_name["TYPE_INT64"].number
+    TYPE_INT32 = enum_descriptor.values_by_name["TYPE_INT32"].number
+    TYPE_INT16 = enum_descriptor.values_by_name["TYPE_INT16"].number
+    TYPE_INT8 = enum_descriptor.values_by_name["TYPE_INT8"].number
+    TYPE_UINT64 = enum_descriptor.values_by_name["TYPE_UINT64"].number
+    TYPE_UINT32 = enum_descriptor.values_by_name["TYPE_UINT32"].number
+    TYPE_UINT16 = enum_descriptor.values_by_name["TYPE_UINT16"].number
+    TYPE_UINT8 = enum_descriptor.values_by_name["TYPE_UINT8"].number
+    TYPE_BOOL = enum_descriptor.values_by_name["TYPE_BOOL"].number
+    TYPE_STRING = enum_descriptor.values_by_name["TYPE_STRING"].number
