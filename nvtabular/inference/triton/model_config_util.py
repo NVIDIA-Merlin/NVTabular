6c9cd6947bdd553753d2c073e1fe2ad17422041a
Julio Perez <jperez@nvidia.com>
2022-05-25 19:44:32 -0400

    add datatype

--- nvtabular/inference/triton/model_config_util.py
+++ nvtabular/inference/triton/model_config_util.py
@@ -8,17 +8,20 @@ if file_descriptor:
     ModelConfig = msg_factory.CreatePrototype(file_descriptor.message_types_by_name["ModelConfig"])
     ModelInput = msg_factory.CreatePrototype(file_descriptor.message_types_by_name["ModelInput"])
     ModelOutput = msg_factory.CreatePrototype(file_descriptor.message_types_by_name["ModelOutput"])
-    enum_descriptor = file_descriptor.enum_types_by_name["DataType"]
-    TYPE_FP64 = enum_descriptor.values_by_name["TYPE_FP64"].number
-    TYPE_FP32 = enum_descriptor.values_by_name["TYPE_FP32"].number
-    TYPE_FP16 = enum_descriptor.values_by_name["TYPE_FP16"].number
-    TYPE_INT64 = enum_descriptor.values_by_name["TYPE_INT64"].number
-    TYPE_INT32 = enum_descriptor.values_by_name["TYPE_INT32"].number
-    TYPE_INT16 = enum_descriptor.values_by_name["TYPE_INT16"].number
-    TYPE_INT8 = enum_descriptor.values_by_name["TYPE_INT8"].number
-    TYPE_UINT64 = enum_descriptor.values_by_name["TYPE_UINT64"].number
-    TYPE_UINT32 = enum_descriptor.values_by_name["TYPE_UINT32"].number
-    TYPE_UINT16 = enum_descriptor.values_by_name["TYPE_UINT16"].number
-    TYPE_UINT8 = enum_descriptor.values_by_name["TYPE_UINT8"].number
-    TYPE_BOOL = enum_descriptor.values_by_name["TYPE_BOOL"].number
-    TYPE_STRING = enum_descriptor.values_by_name["TYPE_STRING"].number
+    ModelEnsembling = msg_factory.CreatePrototype(
+        file_descriptor.message_types_by_name["ModelEnsembling"]
+    )
+    _DATATYPE = file_descriptor.enum_types_by_name["DataType"]
+    TYPE_FP64 = _DATATYPE.values_by_name["TYPE_FP64"].number
+    TYPE_FP32 = _DATATYPE.values_by_name["TYPE_FP32"].number
+    TYPE_FP16 = _DATATYPE.values_by_name["TYPE_FP16"].number
+    TYPE_INT64 = _DATATYPE.values_by_name["TYPE_INT64"].number
+    TYPE_INT32 = _DATATYPE.values_by_name["TYPE_INT32"].number
+    TYPE_INT16 = _DATATYPE.values_by_name["TYPE_INT16"].number
+    TYPE_INT8 = _DATATYPE.values_by_name["TYPE_INT8"].number
+    TYPE_UINT64 = _DATATYPE.values_by_name["TYPE_UINT64"].number
+    TYPE_UINT32 = _DATATYPE.values_by_name["TYPE_UINT32"].number
+    TYPE_UINT16 = _DATATYPE.values_by_name["TYPE_UINT16"].number
+    TYPE_UINT8 = _DATATYPE.values_by_name["TYPE_UINT8"].number
+    TYPE_BOOL = _DATATYPE.values_by_name["TYPE_BOOL"].number
+    TYPE_STRING = _DATATYPE.values_by_name["TYPE_STRING"].number
