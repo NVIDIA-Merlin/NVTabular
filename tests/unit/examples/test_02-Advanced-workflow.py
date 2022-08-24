7e378b00d618366cea5b1a869b0b568fe8ec3c23
Radek Osmulski <rosmulski@gmail.com>
2022-08-25 09:55:57 +1000

    add unit test for 02

---
+++ tests/unit/examples/test_02-Advanced-workflow.py
@@ -0,0 +1,88 @@
+#
+# Copyright (c) 2021, NVIDIA CORPORATION.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+from testbook import testbook
+
+from tests.conftest import REPO_ROOT
+
+
+@testbook(REPO_ROOT / "examples/02-Advanced-workflow.ipynb", execute=False)
+def test_example_02_advanced_workflow(tb):
+    tb.inject(
+        """
+        import os
+        from unittest.mock import patch
+        from merlin.datasets.synthetic import generate_data
+        mock_train, mock_valid = generate_data(
+            input="movielens-1m",
+            num_rows=1000,
+            set_sizes=(0.8, 0.2)
+        )
+        input_path = os.environ.get(
+            "INPUT_DATA_DIR",
+            os.path.expanduser("~/merlin-framework/movielens/")
+        )
+        mock_train.compute().to_parquet(f'{input_path}/train.parquet')
+
+        p1 = patch(
+            "merlin.datasets.entertainment.get_movielens",
+            return_value=[mock_train, mock_valid]
+        )
+        p1.start()
+        """
+    )
+    tb.execute_cell(range(7))
+    tb.inject(
+        """
+            import cudf
+            import string
+            import numpy as np
+
+            def generate_title():
+                letters = list(string.ascii_letters)
+                np.random.shuffle(letters)
+                return ''.join(letters)[:np.random.randint(len(letters))]
+
+            train = cudf.read_parquet(f'{input_path}/train.parquet')
+            valid = cudf.read_parquet(f'{input_path}/train.parquet')
+
+            num_rows = train.movieId.unique().size
+            movies = cudf.DataFrame(
+                data={
+                    'movieId': train.movieId.unique(),
+                    'title': [generate_title() for i in range(num_rows)],
+                    'genres': [['a_genre'] for i in range(num_rows)]}
+            )
+            """
+    )
+    tb.execute_cell(range(8, len(tb.cells)))
+    metrics = tb.ref("metrics")
+    assert set(metrics.history.keys()) == set(
+        [
+            "auc_1",
+            "binary_accuracy",
+            "loss",
+            "precision_1",
+            "recall_1",
+            "regularization_loss",
+            "val_auc_1",
+            "val_binary_accuracy",
+            "val_loss",
+            "val_precision_1",
+            "val_recall_1",
+            "val_regularization_loss",
+        ]
+    )
