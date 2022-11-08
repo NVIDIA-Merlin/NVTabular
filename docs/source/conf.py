bd8a00a62046df412e092760b0756d5710dc525c
Mike McKiernan <mmckiernan@nvidia.com>
2022-11-08 08:59:02 -0500

    docs: Add semver to calver banner
    All Merlin projects are switching from semantic versioning,
    like `0.4.0`, to calendar versioning, like `23.01`.
    The change is planned for January 2023.
    Temporarily add a banner to all pages to prepare
    our consumers for the change.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -95,7 +95,8 @@ html_show_sourcelink = False
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = []
+html_static_path = ["_static"]
+html_css_files = ["css/custom.css"]
 
 source_suffix = [".rst", ".md"]
 
