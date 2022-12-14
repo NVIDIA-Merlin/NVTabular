61c4a10e714a7ce9f8d276f10d94a0b5b9d387da
Oliver Holworthy <oholworthy@nvidia.com>
2022-12-14 19:01:37 +0000

    Add missing colons for error message

--- ci/check_dist.sh
+++ ci/check_dist.sh
@@ -9,7 +9,7 @@ pkginfo -f requires_dist ${__root}/dist/*.whl | grep "@"
 
 if [ $? -eq 0 ]; then
     cat <<EOF
-::error Found direct reference(s) in package requires_dist. Please use only normal version specifiers.
+::error::Found direct reference(s) in package requires_dist. Please use only normal version specifiers.
 See https://peps.python.org/pep-0440/#direct-references for more details.
 EOF
     exit 1
