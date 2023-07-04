77b94a40babfea160130c70160dfdf60356b4f16
Oliver Holworthy <oholworthy@nvidia.com>
2023-07-04 11:04:35 +0100

    Add Pytest Markers Configuration (#1849)
    * Add pytest.ini with markers specified
    
    * Update conftest with automatic markers based on test path
    
    ---------
    
    Co-authored-by: Karl Higley <kmhigley@gmail.com>

--- tests/conftest.py
+++ tests/conftest.py
@@ -374,6 +374,29 @@ def report(request):
     return request.config.getoption("--report")
 
 
+def pytest_collection_modifyitems(items):
+    for item in items:
+        path = item.location[0]
+
+        if "/unit/" in path:
+            item.add_marker(getattr(pytest.mark, "unit"))
+
+        if "/loader/" in path:
+            item.add_marker(getattr(pytest.mark, "loader"))
+
+        if "/examples/" in path:
+            item.add_marker(getattr(pytest.mark, "examples"))
+
+        if "/ops/" in path:
+            item.add_marker(getattr(pytest.mark, "ops"))
+
+        if "test_tf_" in path:
+            item.add_marker(getattr(pytest.mark, "tensorflow"))
+
+        if "test_torch_" in path:
+            item.add_marker(getattr(pytest.mark, "torch"))
+
+
 @pytest.fixture(scope="function", autouse=True)
 def cleanup_dataloader():
     """After each test runs. Call .stop() on any dataloaders created during the test.
