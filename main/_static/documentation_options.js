b059d2e1a5f31e5f20f5954c600dab685c47fb25
docs-sched-rebuild <do-not-send-@github.com>
2024-05-02 16:20:02 +0000

    Pushing changes to GitHub Pages.

--- main/_static/documentation_options.js
+++ main/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2021',
+    VERSION: '',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
