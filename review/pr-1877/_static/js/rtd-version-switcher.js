9633776e4f41a5c3c65c9a8e2fd37164193c2b98
docs-remove-stale-reviews <do-not-send-@github.com>
2024-05-05 01:03:49 +0000

    Removing stale reviews from GitHub Pages.

--- review/pr-1877/_static/js/rtd-version-switcher.js
+++
@@ -1,5 +0,0 @@
-var jQuery = (typeof(window) != 'undefined') ? window.jQuery : require('jquery');
-var doc = $(document);
-doc.on('click', "[data-toggle='rst-current-version']", function() {
-    $("[data-toggle='rst-versions']").toggleClass("shift-up");
-});
